9dd90751aa4e252e70a644f32358e094086e1202
sven-n <sven-n@users.noreply.github.com>
2018-01-10 23:57:18 +0100

    Added some tests for the json query builder
    Should help when investigating #11.

--- src/Persistence/EntityFramework/Properties/AssemblyInfo.cs
+++ src/Persistence/EntityFramework/Properties/AssemblyInfo.cs
@@ -3,8 +3,10 @@
 // </copyright>
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MUnique.OpenMU.Persistence.EntityFramework")]
+[assembly: InternalsVisibleTo("MUnique.OpenMU.Persistence.Initialization.Tests")]
---
+++ tests/MUnique.OpenMU.Persistence.Initialization.Tests/JsonQueryBuilderTests.cs
@@ -0,0 +1,107 @@
+﻿// <copyright file="JsonQueryBuilderTests.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Persistence.Initialization.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
+    using MUnique.OpenMU.Persistence.EntityFramework;
+    using MUnique.OpenMU.Persistence.EntityFramework.Json;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for the <see cref="JsonQueryBuilder"/>.
+    /// </summary>
+    [TestFixture]
+    internal class JsonQueryBuilderTests
+    {
+        /// <summary>
+        /// Tests the json query builder for the <see cref="GameConfiguration"/> type.
+        /// </summary>
+        [Test]
+        public void JsonQueryBuilderGameConfiguration()
+        {
+            using (var installationContext = new ConfigurationContext())
+            {
+                var type = installationContext.Model.GetEntityTypes().FirstOrDefault(t => t.ClrType == typeof(GameConfiguration));
+                string result;
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                try
+                {
+                    var builder = new JsonQueryBuilder();
+                    result = builder.BuildJsonQueryForEntity(type);
+                }
+                finally
+                {
+                    stopwatch.Stop();
+                }
+
+                result = $"-- Json query created in {stopwatch.ElapsedMilliseconds} ms:{Environment.NewLine}" + result;
+                File.WriteAllText(@"C:\temp\json_GameConfiguration.txt", result);
+            }
+        }
+
+        /// <summary>
+        /// Tests the json query builder for the <see cref="Account"/> type.
+        /// </summary>
+        [Test]
+        public void JsonQueryBuilderAccount()
+        {
+            using (var installationContext = new ConfigurationContext())
+            {
+                var type = installationContext.Model.GetEntityTypes().FirstOrDefault(t => t.ClrType == typeof(Account));
+                string result;
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                try
+                {
+                    var builder = new AccountJsonQueryBuilder();
+                    result = builder.BuildJsonQueryForEntity(type);
+                }
+                finally
+                {
+                    stopwatch.Stop();
+                }
+
+                result = $"-- Json query created in {stopwatch.ElapsedMilliseconds} ms:{Environment.NewLine}" + result;
+                File.WriteAllText(@"C:\temp\json_Account.txt", result);
+            }
+        }
+
+        /// <summary>
+        /// Loads the <see cref="GameConfiguration"/> using the <see cref="JsonQueryBuilder"/> and the <see cref="JsonObjectLoader"/>.
+        /// It always fails, because it reports the taken time.
+        /// </summary>
+        [Test]
+        [Ignore("It hits the database.")]
+        public void LoadConfigByJson()
+        {
+            using (var installationContext = new ConfigurationContext())
+            {
+                installationContext.Database.OpenConnection();
+                var builder = new JsonObjectLoader();
+                IEnumerable<GameConfiguration> result;
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                try
+                {
+                    result = builder.LoadAllObjects<EntityFramework.GameConfiguration>(installationContext).ToList();
+                }
+                finally
+                {
+                    stopwatch.Stop();
+                }
+
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result.Count, Is.Not.EqualTo(0));
+                Assert.That(stopwatch.ElapsedMilliseconds, Is.EqualTo(0));
+            }
+        }
+    }
+}
