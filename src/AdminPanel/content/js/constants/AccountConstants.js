378480dfd4b9172900c9c1f5f2b57e7dc809c1ca
sven-n <sven-n@users.noreply.github.com>
2018-01-21 12:54:54 +0100

    Added Account related constants

---
+++ src/AdminPanel/content/js/constants/AccountConstants.js
@@ -0,0 +1,34 @@
+﻿var AccountState = {
+
+    Normal: 0,
+
+    Spectator: 1,
+
+    GameMaster: 2,
+
+    GameMasterInvisible: 3,
+
+    Banned: 4,
+
+    TemporarilyBanned: 5,
+
+
+    getCaption: function (state) {
+        switch (state) {
+        case AccountState.Normal:
+            return "Normal";
+        case AccountState.Banned:
+            return "Banned";
+        case AccountState.GameMaster:
+            return "Game Master";
+        case AccountState.Spectator:
+            return "Spectator";
+        case AccountState.GameMasterInvisible:
+            return "Invisible GameMaster";
+        case AccountState.TemporarilyBanned:
+            return "Temporarily Banned";
+        }
+
+        return "";
+    }
+}
