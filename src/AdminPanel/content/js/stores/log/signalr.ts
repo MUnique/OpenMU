736b5ae2a3e3496599c19e19d3641404a97c4ab5
sven-n <sven-n@users.noreply.github.com>
2018-06-13 18:14:29 +0200

    Added missing log folder - they were ignored by git

---
+++ src/AdminPanel/content/js/stores/log/signalr.ts
@@ -0,0 +1,83 @@
+﻿import Redux, { MiddlewareAPI, Middleware, Dispatch, Action } from "redux";
+import Store = Redux.Store;
+import { LogEntryData } from "./types";
+import { ApplicationState } from "../index";
+import { SignalRConnector, HubProxy, HubServer } from "../signalr";
+import { Constants, logInitialize, logEvent, LogSubscribeAction, LogUnsubscribeAction } from "./actions";
+
+
+declare var $: { connection: { fooBarHub: LogHubProxy } };
+
+interface LogEventArgs {
+    Id: number;
+    FormattedEvent: string;
+    LoggingEvent: LogEntryData;
+}
+
+interface LogHubClient {
+    initialize: (loggers: string[], cachedEvents: LogEventArgs[]) => void;
+    onLoggedEvent: (formattedEvent: any, logEvent: LogEntryData) => void;
+}
+
+interface LogHubProxy extends HubProxy {
+    client: LogHubClient;
+    server: LogHubServer;
+}
+
+interface LogHubServer extends HubServer {
+    subscribe(): void;
+    subscribe(group: string, idOfLastReceivedEntry: number): void;
+}
+
+export var logTableConnector: LogTableSignalRConnector;
+
+class LogTableSignalRConnector extends SignalRConnector<LogHubProxy> {
+
+    constructor(store: Store<ApplicationState>) {
+        super(store);
+    }
+
+    onFirstSubscription(): void {
+        this.hub.server.subscribe('MyGroup', this.store.getState().logTableState.idOfLastReceivedEntry);
+    }
+
+    protected initializeHub(): LogHubProxy {
+        var hubProxy = $.connection.fooBarHub;
+        hubProxy.client.initialize = (loggers, cachedEvents) => this.initialize(loggers, cachedEvents);
+        hubProxy.client.onLoggedEvent = (formattedEvent, loggedEvent) => this.onLoggedEvent(formattedEvent, loggedEvent);
+        return hubProxy;
+    }
+
+    private initialize(loggers: string[], cachedEvents: LogEventArgs[]) {
+        if (console && console.log) {
+            console.log("log hub: init");
+        }
+
+        this.store.dispatch(logInitialize(loggers, cachedEvents.map(event => { return event.LoggingEvent })));
+    }
+
+    private onLoggedEvent(formattedEvent: any, loggedEvent: LogEntryData) {
+        if (console && console.log) {
+            console.log("onLoggedEvent", formattedEvent, loggedEvent);
+        }
+
+        this.store.dispatch(logEvent(loggedEvent));
+    }
+}
+
+export const logTableConnectorMiddleware: Middleware =
+    (api: MiddlewareAPI) =>
+        (next: Dispatch) =>
+        (action: Action) => {
+            if (action.type === Constants.LOG_SUBSCRIBE) {
+                logTableConnector.subscribe((action as LogSubscribeAction).subscriber);
+            } else if (action.type === Constants.LOG_UNSUBSCRIBE) {
+                logTableConnector.unsubscribe((action as LogUnsubscribeAction).subscriber);
+            } else {
+                next(action);
+            }
+        };
+
+export function configureLogTableConnector(store: Store<ApplicationState>): void {
+    logTableConnector = new LogTableSignalRConnector(store);
+}
---
+++ src/AdminPanel/content/js/stores/log/types.ts
@@ -0,0 +1,28 @@
+﻿// ReSharper disable InconsistentNaming naming is caused by C# classes which are serialized like that.
+
+export interface LogEntryData {
+    readonly Id: number;
+    readonly TimeStamp: string;
+    readonly LoggerName: string;
+    readonly Message: string;
+    readonly ExceptionString: string;
+    readonly ThreadName: string;
+    readonly Level: LogLevel;
+    readonly Properties: { [key: string]: string };
+    readonly LocationInfo: LogLocation;
+}
+export interface LogLevel {
+    readonly Name: string;
+}
+
+export interface LogLocation extends StackLocation {
+    readonly MethodName: string;
+    readonly StackFrames: StackLocation[];
+}
+
+export interface StackLocation {
+    readonly ClassName: string;
+    readonly FileName: string;
+    readonly LineNumber: number;
+    readonly FullInfo: string;
+}
