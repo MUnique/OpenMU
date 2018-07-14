5402eb6c02a4c9e70b194d1b28cd55c744b9ac07
ThisMushroom <thismushroom@gmail.com>
2018-07-15 01:45:08 +0300

    Store Work Part 1
    Added ItemHelper to create items
    Added [Lorencia] Pasi The Mage Store (Global like)
    Added [Lorencia] Hanzo The Blacksmith Store (Global like)
    Added [Lorencia] Wandering Merchant Store (Global like)
    [WIP] [Lorencia] Potion Girl Store (Global like)
    [WIP] NpcInitialization - Maybe create separate store initialization system.
    [WIP] ItemHelper - Maybe add some enums for more readability in item creation process.
    [WIP] ItemHelper - Further refactoring of ItemHelper (make it more consistant, remove code duplicates).

--- src/Persistence/Initialization/NpcInitialization.cs
+++ src/Persistence/Initialization/NpcInitialization.cs
@@ -4,9 +4,11 @@
 
 namespace MUnique.OpenMU.Persistence.Initialization
 {
+    using System.Collections.Generic;
     using System.Linq;
     using MUnique.OpenMU.DataModel.Configuration;
     using MUnique.OpenMU.DataModel.Entities;
+    using MUnique.OpenMU.Persistence.Initialization.Items;
 
     /// <summary>
     /// The initialization of all NPCs, which are no monsters.
@@ -17,6 +19,8 @@ namespace MUnique.OpenMU.Persistence.Initialization
 
         private readonly GameConfiguration gameConfiguration;
 
+        private ItemHelper itemHelper;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="NpcInitialization" /> class.
         /// </summary>
@@ -26,6 +30,7 @@ namespace MUnique.OpenMU.Persistence.Initialization
         {
             this.context = context;
             this.gameConfiguration = gameConfiguration;
+            this.itemHelper = new ItemHelper(this.context, this.gameConfiguration);
         }
 
         /// <summary>
@@ -203,7 +208,7 @@ namespace MUnique.OpenMU.Persistence.Initialization
                 def.Number = 250;
                 def.Designation = "Wandering Merchant";
                 def.NpcWindow = NpcWindow.Merchant;
-                def.MerchantStore = this.CreateMerchantStoreWithPotions();
+                def.MerchantStore = this.CreateWanderingMerchant();
                 this.gameConfiguration.Monsters.Add(def);
             }
 
@@ -212,6 +217,7 @@ namespace MUnique.OpenMU.Persistence.Initialization
                 def.Number = 251;
                 def.Designation = "Hanzo The Blacksmith";
                 def.NpcWindow = NpcWindow.Merchant;
+                def.MerchantStore = this.CreateHanzoTheBlacksmith();
                 this.gameConfiguration.Monsters.Add(def);
             }
 
@@ -220,7 +226,7 @@ namespace MUnique.OpenMU.Persistence.Initialization
                 def.Number = 253;
                 def.Designation = "Potion Girl";
                 def.NpcWindow = NpcWindow.Merchant;
-                def.MerchantStore = this.CreateMerchantStoreWithPotions();
+                def.MerchantStore = this.CreatePotionGirl();
                 this.gameConfiguration.Monsters.Add(def);
             }
 
@@ -229,6 +235,7 @@ namespace MUnique.OpenMU.Persistence.Initialization
                 def.Number = 254;
                 def.Designation = "Pasi The Mage";
                 def.NpcWindow = NpcWindow.Merchant;
+                def.MerchantStore = this.CreatePasiTheMageStore();
                 this.gameConfiguration.Monsters.Add(def);
             }
 
@@ -237,7 +244,7 @@ namespace MUnique.OpenMU.Persistence.Initialization
                 def.Number = 255;
                 def.Designation = "Lumen The Barmaid";
                 def.NpcWindow = NpcWindow.Merchant;
-                def.MerchantStore = this.CreateMerchantStoreWithPotions();
+                def.MerchantStore = this.CreatePotionGirl();
                 this.gameConfiguration.Monsters.Add(def);
             }
 
@@ -969,6 +976,179 @@ namespace MUnique.OpenMU.Persistence.Initialization
                 def.Designation = "Captured Stone Statue (10)";
                 this.gameConfiguration.Monsters.Add(def);
             }
+        }
+
+        private ItemStorage CreatePotionGirl()
+        {
+            List<Item> itemList = new List<Item>();
+
+            itemList.Add(this.itemHelper.CreatePotion(0, 0, 1, 0));     // Apple +0 x1
+            itemList.Add(this.itemHelper.CreatePotion(8, 0, 3, 0));     // Apple +0 x3
+            itemList.Add(this.itemHelper.CreatePotion(16, 0, 1, 1));    // Apple +1 x1
+            itemList.Add(this.itemHelper.CreatePotion(24, 0, 3, 1));    // Apple +1 x3
+
+            itemList.Add(this.itemHelper.CreatePotion(1, 1, 1, 0));     // Small Healing Potion +0 x1
+            itemList.Add(this.itemHelper.CreatePotion(9, 1, 3, 0));     // Small Healing Potion +0 x3
+            itemList.Add(this.itemHelper.CreatePotion(17, 1, 1, 1));    // Small Healing Potion +1 x1
+            itemList.Add(this.itemHelper.CreatePotion(25, 1, 3, 1));    // Small Healing Potion +1 x3
+
+            itemList.Add(this.itemHelper.CreatePotion(2, 2, 1, 0));     // Medium Healing Potion +0 x1
+            itemList.Add(this.itemHelper.CreatePotion(10, 2, 3, 0));    // Medium Healing Potion +0 x3
+            itemList.Add(this.itemHelper.CreatePotion(18, 2, 1, 1));    // Medium Healing Potion +1 x1
+            itemList.Add(this.itemHelper.CreatePotion(26, 2, 3, 1));    // Medium Healing Potion +1 x3
+
+            itemList.Add(this.itemHelper.CreatePotion(3, 3, 1, 0));     // Large Healing Potion +0 x1
+            itemList.Add(this.itemHelper.CreatePotion(11, 3, 3, 0));    // Large Healing Potion +0 x3
+            itemList.Add(this.itemHelper.CreatePotion(19, 3, 1, 1));    // Large Healing Potion +1 x1
+            itemList.Add(this.itemHelper.CreatePotion(27, 3, 3, 1));    // Large Healing Potion +1 x3
+
+            itemList.Add(this.itemHelper.CreatePotion(4, 4, 1, 0));     // Small Mana Potion +0 x1
+            itemList.Add(this.itemHelper.CreatePotion(12, 4, 3, 0));    // Small Mana Potion +0 x3
+            itemList.Add(this.itemHelper.CreatePotion(20, 4, 1, 1));    // Small Mana Potion +1 x1
+            itemList.Add(this.itemHelper.CreatePotion(28, 4, 3, 1));    // Small Mana Potion +1 x3
+
+            itemList.Add(this.itemHelper.CreatePotion(5, 5, 1, 0));     // Medium Mana Potion +0 x1
+            itemList.Add(this.itemHelper.CreatePotion(13, 5, 3, 0));    // Medium Mana Potion +0 x3
+            itemList.Add(this.itemHelper.CreatePotion(21, 5, 1, 1));    // Medium Mana Potion +1 x1
+            itemList.Add(this.itemHelper.CreatePotion(29, 5, 3, 1));    // Medium Mana Potion +1 x3
+
+            itemList.Add(this.itemHelper.CreatePotion(6, 6, 1, 0));     // Large Mana Potion +0 x1
+            itemList.Add(this.itemHelper.CreatePotion(14, 6, 3, 0));    // Large Mana Potion +0 x3
+            itemList.Add(this.itemHelper.CreatePotion(22, 6, 1, 1));    // Large Mana Potion +1 x1
+            itemList.Add(this.itemHelper.CreatePotion(30, 6, 3, 1));    // Large Mana Potion +1 x3
+
+            itemList.Add(this.itemHelper.CreatePotion(7, 9, 1, 0));     // Ale +0 x1
+            // TODO: insert Town Portal item.
+            // TODO: insert Ammo (Bolts and Arrows).
+            // TODO: insert Armor of Guardsman.
+            return this.CreateMerchantStore(itemList);
+        }
+
+        private ItemStorage CreateWanderingMerchant()
+        {
+            List<Item> itemList = new List<Item>();
+
+            itemList.Add(this.itemHelper.CreateSetItem(0, 5, 7, null, 0, 1, true));     // Leather Helm     +0+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(16, 5, 8, null, 0, 1, true));    // Leather Armor    +0+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(40, 5, 9, null, 0, 1, true));    // Leather Pants    +0+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(56, 5, 11, null, 0, 1, true));   // Leather Boots    +0+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(72, 5, 10, null, 0, 1, true));   // Leather Gloves   +0+4+L
+
+            itemList.Add(this.itemHelper.CreateSetItem(2, 0, 7, null, 2, 1, true));     // Bronze Helm    +2+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(18, 0, 8, null, 2, 1, true));    // Bronze Armor   +2+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(34, 0, 9, null, 2, 1, true));    // Bronze Pants   +2+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(50, 0, 11, null, 2, 1, true));   // Bronze Boots   +2+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(66, 0, 10, null, 2, 1, true));   // Bronze Gloves  +2+4+L
+
+            itemList.Add(this.itemHelper.CreateSetItem(4, 6, 7, null, 3, 1, true));     // Scale Helm      +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(20, 6, 8, null, 3, 1, true));    // Scale Armor     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(36, 6, 9, null, 3, 1, true));    // Scale Pants     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(52, 6, 11, null, 3, 1, true));   // Scale Boots     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(68, 6, 10, null, 3, 1, true));   // Scale Gloves    +3+4+L
+
+            itemList.Add(this.itemHelper.CreateSetItem(6, 8, 7, null, 3, 1, true));     // Brass Helm      +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(22, 8, 8, null, 3, 1, true));    // Brass Armor     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(38, 8, 9, null, 3, 1, true));    // Brass Pants     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(54, 8, 11, null, 3, 1, true));   // Brass Boots     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(70, 8, 10, null, 3, 1, true));   // Brass Gloves    +3+4+L
+
+            itemList.Add(this.itemHelper.CreateSetItem(88, 9, 7, null, 3, 1, true));    // Plate Helm      +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(104, 9, 8, null, 3, 1, true));   // Plate Armor     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(82, 9, 9, null, 3, 1, true));    // Plate Pants     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(98, 9, 11, null, 3, 1, true));   // Plate Boots     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(84, 9, 10, null, 3, 1, true));   // Plate Gloves    +3+4+L
+
+            return this.CreateMerchantStore(itemList);
+        }
+
+        private ItemStorage CreateHanzoTheBlacksmith()
+        {
+            List<Item> itemList = new List<Item>();
+
+            itemList.Add(this.itemHelper.CreateShield(0, 0, false, null, 0, 1, true));  // Small Shield     +0+4+L
+            itemList.Add(this.itemHelper.CreateShield(2, 4, true, null, 1, 1, true));   // Buckler          +1+4+L+S
+            itemList.Add(this.itemHelper.CreateShield(4, 1, false, null, 2, 1, true));  // Horn Shield      +2+4+L
+            itemList.Add(this.itemHelper.CreateShield(6, 2, false, null, 3, 1, true));  // Kite Shield      +3+4+L
+
+            itemList.Add(this.itemHelper.CreateShield(16, 6, true, null, 3, 1, true));  // Skull Shield     +3+4+L+S
+            itemList.Add(this.itemHelper.CreateShield(18, 10, true, null, 3, 1, true)); // Big Round Shield +3+4+L+S
+            itemList.Add(this.itemHelper.CreateShield(20, 9, true, null, 3, 1, true));  // Plate Shield     +3+4+L+S
+            itemList.Add(this.itemHelper.CreateShield(22, 7, true, null, 3, 1, true));  // Spiked Shield    +3+4+L+S
+
+            itemList.Add(this.itemHelper.CreateShield(32, 5, true, null, 3, 1, true));  // Dragon Slayer    +3+4+L+S
+            itemList.Add(this.itemHelper.CreateShield(34, 8, true, null, 3, 1, true));  // Tower Shield     +3+4+L+S
+            itemList.Add(this.itemHelper.CreateShield(36, 11, true, null, 3, 1, true)); // Serpent Shield   +3+4+L+S
+            itemList.Add(this.itemHelper.CreateShield(38, 12, true, null, 3, 1, true)); // Bronze Shield    +3+4+L+S
+
+            itemList.Add(this.itemHelper.CreateWeapon(48, 0, 1, 0, 1, true, false, null));  // Short Sword  +0+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(49, 1, 1, 1, 1, true, false, null));  // Hand Axe     +1+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(50, 2, 1, 2, 1, true, false, null));  // Mace         +2+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(51, 0, 2, 2, 1, true, false, null));  // Rapier       +2+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(52, 1, 2, 2, 1, true, false, null));  // Double Axe   +2+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(53, 0, 4, 3, 1, true, true, null));   // Assassin     +3+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(54, 2, 1, 3, 1, true, true, null));   // Morning Star +3+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(55, 1, 3, 3, 1, true, true, null));   // Tomahawk     +3+4+L
+
+            itemList.Add(this.itemHelper.CreateWeapon(72, 0, 0, 2, 1, true, false, null));  // Kris             +2+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(73, 0, 6, 3, 1, true, true, null));   // Gladius          +3+4+L+S
+            itemList.Add(this.itemHelper.CreateWeapon(73, 0, 7, 3, 1, true, true, null));   // Falchion         +3+4+L+S
+            itemList.Add(this.itemHelper.CreateWeapon(74, 0, 8, 3, 1, true, false, null));  // Serpent Sword    +3+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(75, 0, 5, 3, 1, true, true, null));   // Blade            +3+4+L+S
+
+            return this.CreateMerchantStore(itemList);
+        }
+
+        private ItemStorage CreatePasiTheMageStore()
+        {
+            List<Item> itemList = new List<Item>();
+
+            itemList.Add(this.itemHelper.CreateScroll(0, 3));   // Scroll of Fire Ball
+            itemList.Add(this.itemHelper.CreateScroll(1, 10));  // Scroll of Power Wave
+            itemList.Add(this.itemHelper.CreateScroll(2, 2));   // Scroll of Lighting
+            itemList.Add(this.itemHelper.CreateScroll(3, 1));   // Scroll of Meteorite
+            itemList.Add(this.itemHelper.CreateScroll(4, 5));   // Scroll of Teleport
+            itemList.Add(this.itemHelper.CreateScroll(5, 6));   // Scroll of Ice
+            itemList.Add(this.itemHelper.CreateScroll(6, 0));   // Scroll of Poison
+
+            itemList.Add(this.itemHelper.CreateOrb(7, 13));     // Orb of Implate
+            itemList.Add(this.itemHelper.CreateOrb(15, 7));     // Orb of Twisting Slash
+
+            itemList.Add(this.itemHelper.CreateSetItem(16, 2, 7, null, 0, 1, true));    // Pad Helm     +0+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(32, 2, 8, null, 0, 1, true));    // Pad Armor    +0+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(48, 2, 9, null, 0, 1, true));    // Pad Pants    +0+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(64, 2, 11, null, 0, 1, true));   // Pad Boots    +0+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(80, 2, 10, null, 0, 1, true));   // Pad Gloves   +0+4+L
+
+            itemList.Add(this.itemHelper.CreateSetItem(18, 4, 7, null, 2, 1, true));    // Bone Helm    +2+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(34, 4, 8, null, 2, 1, true));    // Bone Armor   +2+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(50, 4, 9, null, 2, 1, true));    // Bone Pants   +2+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(66, 4, 11, null, 2, 1, true));   // Bone Boots   +2+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(82, 4, 10, null, 2, 1, true));   // Bone Gloves  +2+4+L
+
+            itemList.Add(this.itemHelper.CreateSetItem(20, 7, 7, null, 3, 1, true));    // Sphinx Helm      +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(36, 7, 8, null, 3, 1, true));    // Sphinx Armor     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(60, 7, 9, null, 3, 1, true));    // Sphinx Pants     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(76, 7, 11, null, 3, 1, true));   // Sphinx Boots     +3+4+L
+            itemList.Add(this.itemHelper.CreateSetItem(92, 7, 10, null, 3, 1, true));   // Sphinx Gloves    +3+4+L
+
+            itemList.Add(this.itemHelper.CreateWeapon(22, 5, 0, 0, 1, true, false, null));  // Skull Staff      +0+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(46, 5, 1, 2, 1, true, false, null));  // Angelic Staff    +2+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(70, 5, 2, 3, 1, true, false, null));  // Serpent Staff    +3+4+L
+            itemList.Add(this.itemHelper.CreateWeapon(94, 5, 3, 3, 1, true, false, null));  // Thunder Staff    +3+4+L
+
+            return this.CreateMerchantStore(itemList);
+        }
+
+        private ItemStorage CreateMerchantStore(List<Item> itemList)
+        {
+            var merchantStore = this.context.CreateNew<ItemStorage>();
+
+            foreach (var item in itemList)
+            {
+                merchantStore.Items.Add(item);
+            }
+
+            return merchantStore;
         }
 
         private ItemStorage CreateMerchantStoreWithPotions()
