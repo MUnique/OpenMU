df5bfb205a039fbce4d7656eaceff400a194c1b6
sven-n <sven-n@users.noreply.github.com>
2018-11-29 21:42:35 +0100

    Refactored code to reduce complexity

--- src/GameLogic/PlayerActions/ItemConsumeActions/ItemConsumeAction.cs
+++ src/GameLogic/PlayerActions/ItemConsumeActions/ItemConsumeAction.cs
@@ -30,31 +30,8 @@ namespace MUnique.OpenMU.GameLogic.PlayerActions.ItemConsumeActions
             var items = gameContext.Configuration.Items.Where(def => !string.IsNullOrEmpty(def.ConsumeHandlerClass));
             foreach (var item in items)
             {
-                var type = Type.GetType(item.ConsumeHandlerClass);
-                if (type != null)
-                {
-                    var constructors = type.GetConstructors();
-                    foreach (var ctor in constructors)
-                    {
-                        var parameters = ctor.GetParameters();
-                        if (!parameters.Any())
-                        {
-                            if (ctor.Invoke(new object[] { }) is IItemConsumeHandler consumeHandler)
-                            {
-                                this.consumeHandlers.Add(item, consumeHandler);
-                                break;
-                            }
-                        }
-                        else if (parameters.Length == 1 && parameters[0].ParameterType == typeof(IPersistenceContextProvider))
-                        {
-                            if (ctor.Invoke(new object[] { gameContext.PersistenceContextProvider }) is IItemConsumeHandler consumeHandler)
-                            {
-                                this.consumeHandlers.Add(item, consumeHandler);
-                                break;
-                            }
-                        }
-                    }
-                }
+                var consumeHandler = this.CreateConsumeHandler(gameContext, item.ConsumeHandlerClass);
+                this.consumeHandlers.Add(item, consumeHandler);
             }
         }
 
@@ -73,8 +50,7 @@ namespace MUnique.OpenMU.GameLogic.PlayerActions.ItemConsumeActions
                 return;
             }
 
-            IItemConsumeHandler consumeHandler;
-            if (!this.consumeHandlers.TryGetValue(item.Definition, out consumeHandler))
+            if (!this.consumeHandlers.TryGetValue(item.Definition, out var consumeHandler))
             {
                 player.PlayerView.RequestedItemConsumptionFailed();
                 return;
@@ -97,64 +73,32 @@ namespace MUnique.OpenMU.GameLogic.PlayerActions.ItemConsumeActions
             }
         }
 
-        /*
-                public ConsumeItemHandler(IGameContext gameContext) : base(gameContext)
+        private IItemConsumeHandler CreateConsumeHandler(IGameContext gameContext, string handlerTypeName)
+        {
+            var handlerType = Type.GetType(handlerTypeName);
+            if (handlerType == null)
+            {
+                throw new ArgumentException($"The consume handler {handlerTypeName} wasn't found.", nameof(handlerTypeName));
+            }
+
+            var constructors = handlerType.GetConstructors();
+            foreach (var ctor in constructors)
+            {
+                var parameters = ctor.GetParameters();
+                if (!parameters.Any())
                 {
-                    // This is specified in the ItemDefinition.ConsumeHandlerClass now:
-                    this.consumeHandlers = new Dictionary<ushort, IItemConsumeHandler>();
-                    this.AddConsumeHandler(0, 0xE0, new AppleConsumeHandler());
-                    this.AddConsumeHandler(1, 0xE0, new SmallHealthPotionConsumeHandler());
-                    this.AddConsumeHandler(2, 0xE0, new MiddleHealthPotionConsumeHandler());
-                    this.AddConsumeHandler(3, 0xE0, new BigHealthPotionConsumeHandler());
-                    this.AddConsumeHandler(4, 0xE0, new SmallManaPotionConsumeHandler());
-                    this.AddConsumeHandler(5, 0xE0, new MiddleManaPotionConsumeHandler());
-                    this.AddConsumeHandler(6, 0xE0, new BigManaPotionConsumeHandler());
-                    this.AddConsumeHandler(9, 0xE0, new AlcoholConsumeHandler());
-                    this.AddConsumeHandler(13, 0xE0, new BlessJewelConsumeHandler());
-                    this.AddConsumeHandler(14, 0xE0, new SoulJewelConsumeHandler());
-                    this.AddConsumeHandler(16, 0xE0, new LifeJewelConsumeHandler());
-                    this.AddConsumeHandler(35, 0xE0, new SmallShieldPotionConsumeHandler());
-                    this.AddConsumeHandler(36, 0xE0, new MiddleShieldPotionConsumeHandler());
-                    this.AddConsumeHandler(37, 0xE0, new LargeShieldPotionConsumeHandler());
-
-                    var learnablesConsumeHandler = new LearnablesConsumeHandler(gameServer);
-                    this.AddConsumeHandler(AllItemsOfAGroup, 0xF0, learnablesConsumeHandler);
-                    this.AddOrbConsumeHandler(learnablesConsumeHandler);
-                    //not implemented yet:
-                    this.AddConsumeHandler(7, 0xE0, new NotImplementedItemConsumeHandler()); //Potion of Soul
-                    this.AddConsumeHandler(8, 0xE0, new NotImplementedItemConsumeHandler()); //Antidote
-                    this.AddConsumeHandler(10, 0xE0, new NotImplementedItemConsumeHandler()); //Town portal scroll
-                    this.AddConsumeHandler(20, 0xE0, new NotImplementedItemConsumeHandler()); //Redemy of love
-                    //Complex Potions:
-                    this.AddConsumeHandler(38, 0xE0, new NotImplementedItemConsumeHandler());
-                    this.AddConsumeHandler(39, 0xE0, new NotImplementedItemConsumeHandler());
-                    this.AddConsumeHandler(40, 0xE0, new NotImplementedItemConsumeHandler());
-                    //Halloween items:
-                    this.AddConsumeHandler(46, 0xE0, new NotImplementedItemConsumeHandler());
-                    this.AddConsumeHandler(47, 0xE0, new NotImplementedItemConsumeHandler());
-                    this.AddConsumeHandler(48, 0xE0, new NotImplementedItemConsumeHandler());
-                    //Cherry Blossom Items:
-                    this.AddConsumeHandler(85, 0xE0, new NotImplementedItemConsumeHandler());
-                    this.AddConsumeHandler(86, 0xE0, new NotImplementedItemConsumeHandler());
-                    this.AddConsumeHandler(87, 0xE0, new NotImplementedItemConsumeHandler());
-
-
-                    this.AddConsumeHandler(15, 0xD0, new NotImplementedItemConsumeHandler()); //Fruits
+                    return ctor.Invoke(new object[] { }) as IItemConsumeHandler
+                           ?? throw new ArgumentException($"The consume handler {handlerTypeName} isn't implementing {nameof(IItemConsumeHandler)}.", nameof(handlerTypeName));
                 }
 
-                void AddOrbConsumeHandler(IItemConsumeHandler learnablesConsumeHandler)
+                if (parameters.Length == 1 && parameters[0].ParameterType == typeof(IPersistenceContextProvider))
                 {
-                    var itemIds = new List<byte>()
-                    {
-                        7, 8, 9, 10, 11, 12, 13, 14,
-                        16, 17, 18, 19, 20, 21, 22, 23, 24,
-                        35, 44, 45, 46, 47, 48
-                    };
-                    foreach (var itemId in itemIds)
-                    {
-                        this.AddConsumeHandler(itemId, 0xC0, learnablesConsumeHandler);
-                    }
+                    return ctor.Invoke(new object[] { gameContext.PersistenceContextProvider }) as IItemConsumeHandler
+                           ?? throw new ArgumentException($"The consume handler {handlerTypeName} isn't implementing {nameof(IItemConsumeHandler)}.", nameof(handlerTypeName));
                 }
-        */
+            }
+
+            throw new ArgumentException($"The consume handler {handlerTypeName} has no suitable constructor. One of '{handlerTypeName}()' or '{handlerTypeName}({nameof(IPersistenceContextProvider)}) is required.", nameof(handlerTypeName));
+        }
     }
 }
