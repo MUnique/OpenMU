817d03fcdf454de2c934599d01366d8000c6cf4d
sven-n <sven-n@users.noreply.github.com>
2018-12-08 19:10:14 +0100

    Ordered usings

--- src/ChatServer/ExDbConnector/Program.cs
+++ src/ChatServer/ExDbConnector/Program.cs
@@ -6,8 +6,8 @@ namespace MUnique.OpenMU.ChatServer.ExDbConnector
 {
     using System;
     using System.IO;
-    using System.Reflection;
     using System.Linq;
+    using System.Reflection;
     using log4net;
     using log4net.Config;
     using MUnique.OpenMU.ChatServer;
