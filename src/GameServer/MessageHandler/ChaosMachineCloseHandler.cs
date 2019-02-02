1afc36fd52973a3d62c64c38ac5872d6bab257f4
andrea.liosi <andrea.liosi@gmail.com>
2019-02-02 14:15:03 +0100

    Add ChaosMachineCloseHandler and edit some headers

---
+++ src/GameServer/MessageHandler/ChaosMachineCloseHandler.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="ChaosMachineCloseHandler.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameServer.MessageHandler.Items
+{
+    using System;
+    using MUnique.OpenMU.GameLogic;
+    using MUnique.OpenMU.GameLogic.PlayerActions;
+
+    /// <summary>
+    /// Handler for chaos mix packets.
+    /// </summary>
+    internal class ChaosMachineCloseHandler : IPacketHandler
+    {
+        private readonly CloseNpcDialogAction closeNpcDialogAction = new CloseNpcDialogAction();
+
+        /// <inheritdoc/>
+        public void HandlePacket(Player player, Span<byte> packet)
+        {
+            this.closeNpcDialogAction.CloseNpcDialog(player);
+        }
+    }
+}
