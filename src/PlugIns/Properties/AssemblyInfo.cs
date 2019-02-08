94bc33867ad63da24adf60642cd4fa717908a640
sven-n <sven-n@users.noreply.github.com>
2019-02-08 16:35:29 +0100

    Added new projects for the plugin system assemblies

---
+++ src/PlugIns/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="AssemblyInfo.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MUnique.OpenMU.PlugIns")]
+
+[assembly: InternalsVisibleTo("MUnique.OpenMU.PlugIns.Tests")]
