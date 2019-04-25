0c26cf4688d618c8f595004cc48e272720ece34a
sven-n <sven-n@users.noreply.github.com>
2019-04-25 20:37:20 +0200

    Extracted modal styles to css

--- src/AdminPanel/content/js/components/CreateAccountModal.tsx
+++ src/AdminPanel/content/js/components/CreateAccountModal.tsx
@@ -17,29 +17,9 @@ export interface ICreateAccountModalState {
 class CreateAccountModal extends React.Component<ICreateAccountModalProps, ICreateAccountModalState> {
 
     public render() {
-        const backdropStyle = {
-            position: 'fixed' as 'fixed',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            backgroundColor: 'rgba(0,0,0,0.3)',
-            padding: 50
-        };
-
-        // The modal "window"
-        const modalStyle = {
-            backgroundColor: '#fff',
-            borderRadius: 5,
-            maxWidth: 500,
-            minHeight: 300,
-            margin: '0 auto',
-            padding: 30
-        };
-
         return (
-            <div style={backdropStyle}>
-                <div style={modalStyle}>
+            <div className="modal-backdrop">
+                <div className="modal-window">
                     <h2>Create Account</h2>
                     <form onSubmit={(event: any) => this.submit(event)}>
                         <table>
