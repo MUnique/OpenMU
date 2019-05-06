bd69e7a73aa0222126e34075504aa77a981faa6f
sven-n <sven-n@users.noreply.github.com>
2019-05-06 18:38:56 +0200

    Changed design of the account creation dialog
    it‘s now similar to the connect server creation dislog
    Thanks @fos83

--- src/AdminPanel/content/js/components/CreateAccountModal.tsx
+++ src/AdminPanel/content/js/components/CreateAccountModal.tsx
@@ -22,32 +22,32 @@ class CreateAccountModal extends React.Component<ICreateAccountModalProps, ICrea
             <div>
                 <h2>Create Account</h2>
                 <form onSubmit={(event: any) => this.submit(event)}>
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td>Login Name:</td>
-                                <td><input type="text" maxLength={10} required={true} onChange={(e: InputFormEvent) => this.loginNameChanged(e)}/></td>
-                        </tr>
-                        <tr>
-                            <td>E-Mail Address:</td>
-                                <td><input type="text" required={true} onChange={(e: InputFormEvent) => this.eMailChanged(e)}/></td>
-                        </tr>
-                        <tr>
-                            <td>Password:</td>
-                                <td><input type="password" required={true} maxLength={10} id="password" onChange={(e: InputFormEvent) => this.passwordChanged(e)}/></td>
-                        </tr>
-                        <tr>
-                            <td>State:</td>
-                            <td>TODO</td>
-                        </tr>
-                        </tbody>
-                        <tfoot>
-                        <tr>
-                            <td><button type="submit" className="btn btn-xs btn-success">Save</button></td>
-                            <td><button type="button" className="btn btn-xs btn-warning" onClick={() => this.props.cancel()}>Cancel</button></td>
-                        </tr>
-                        </tfoot>
-                    </table>
+                    <div className="form-group">
+                        <label>Login Name</label>
+                        <input className="form-control" type="text" maxLength={10} required={true} onChange={(e: InputFormEvent) => this.loginNameChanged(e)} />
+                    </div>
+                    <div className="form-group">
+                        <label>E-Mail Address</label>
+                        <input className="form-control" type="text" required={true} onChange={(e: InputFormEvent) => this.eMailChanged(e)} />
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input className="form-control" type="password" required={true} maxLength={10} id="password" onChange={(e: InputFormEvent) => this.passwordChanged(e)} />
+                    </div>
+                    <div className="form-group">
+                        <label>State</label>
+                        <select className="form-control" disabled={true}>
+                            <option>TODO</option>
+                        </select>
+                    </div>
+                    <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups" >
+                        <div className="btn-group" role="group" aria-label="Save group">
+                            <button type="submit" className="btn btn-success btn-secondary">Save</button>
+                        </div>
+                        <div className="btn-group" role="group" aria-label="Cancel group">
+                            {this.props.cancel ? <button type="button" className="btn btn-warning btn-secondary" onClick={() => this.props.cancel()}>Cancel</button> : null}
+                        </div>
+                    </div>
                 </form>
             </div>);
     }
