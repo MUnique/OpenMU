a49e9dd59e276c603385151a931b8eafe2352750
sven-n <sven-n@users.noreply.github.com>
2019-06-07 07:09:15 +0200

    Implemented Earth Shake skill
    To support customizing of skills, a new plugin point has been added.
    For the earth shake skill, I added a plugin which pushes the targets away from the player.

---
+++ src/GameLogic/PlugIns/IAreaSkillPlugIn.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="IAreaSkillPlugIn.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.PlugIns
+{
+    using System.Runtime.InteropServices;
+    using MUnique.OpenMU.DataModel.Entities;
+    using MUnique.OpenMU.Pathfinding;
+    using MUnique.OpenMU.PlugIns;
+
+    /// <summary>
+    /// A plugin interface which is called when a target got attacked by an area skill.
+    /// The key is the skill number.
+    /// </summary>
+    [Guid("BAE1E31E-08EA-4B77-BE0E-89DECD9EAA29")]
+    [PlugInPoint("Area skill plugins", "Is called when a target got attacked by an area skill.")]
+    public interface IAreaSkillPlugIn : IStrategyPlugIn<short>
+    {
+        /// <summary>
+        /// Is called after a target got automatically attacked by an area skill, regardless if the attack effectively hit or not.
+        /// </summary>
+        /// <param name="attacker">The attacker.</param>
+        /// <param name="target">The target.</param>
+        /// <param name="skillEntry">The skill entry.</param>
+        /// <param name="targetAreaCenter">The target area center.</param>
+        void AfterTargetGotAttacked(IAttackable attacker, IAttackable target, SkillEntry skillEntry, Point targetAreaCenter);
+    }
+}
