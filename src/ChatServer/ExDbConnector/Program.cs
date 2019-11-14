d34afc58c6de4e327a1879d9b8527fd1cc8d2f55
thinhnguyenh <thinh.nguyenhung@nashtechglobal.com>
2019-11-15 00:23:28 +0700

    fix issues

--- src/ChatServer/ExDbConnector/Program.cs
+++ src/ChatServer/ExDbConnector/Program.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System.Text.RegularExpressions;
+
 namespace MUnique.OpenMU.ChatServer.ExDbConnector
 {
     using System;
@@ -33,7 +35,8 @@ namespace MUnique.OpenMU.ChatServer.ExDbConnector
         {
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.ConfigureAndWatch(logRepository, new FileInfo(Log4NetConfigFilePath));
-            var addressResolver = args.Contains("-local") ? (IIpAddressResolver)new LocalIpResolver() : new PublicIpResolver();
+            GetIpValue(args, out string ip);
+            var addressResolver = args.Contains("-local") ? (IIpAddressResolver)new LocalIpResolver(ip) : new PublicIpResolver();
             var settings = new Settings("ChatServer.cfg");
 
             int chatServerListenerPort = settings.ChatServerListenerPort ?? 55980;
@@ -61,6 +64,23 @@ namespace MUnique.OpenMU.ChatServer.ExDbConnector
             catch (Exception ex)
             {
                 Log.Fatal("Unexpected error occured", ex);
+            }
+        }
+
+        private static void GetIpValue(string[] args, out string ip)
+        {
+            ip = string.Empty;
+            var keyName = "-ip=";
+            if (args.Any(arg => arg.Contains(keyName)))
+            {
+                Regex cmdRegEx = new Regex($@"{keyName}(?<val>.+)");
+
+                var value = args.First(x => x.Contains(keyName));
+                Match match = cmdRegEx.Match(value);
+                if (match.Success)
+                {
+                    ip = match.Groups["val"].Value;
+                }
             }
         }
     }
