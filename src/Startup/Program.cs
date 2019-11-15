6b65b2519410c059d8dc5c2438b3fa2b13a5ee0e
sven-n <sven-n@users.noreply.github.com>
2019-11-15 19:32:37 +0100

    Removed unused using

--- src/Startup/Program.cs
+++ src/Startup/Program.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using System.Text.RegularExpressions;
-
 namespace MUnique.OpenMU.Startup
 {
     using System;
