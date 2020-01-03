bd3cc782d5a4ec26d7b73bcd1a351b3a8e92bfaf
sven-n <sven-n@users.noreply.github.com>
2020-01-03 20:52:44 +0100

    Fixed Character List 0.75
    Need to pass the HeaderType (C1) instead of the op-code.

--- src/GameServer/RemoteView/Character/ShowCharacterListPlugIn075.cs
+++ src/GameServer/RemoteView/Character/ShowCharacterListPlugIn075.cs
@@ -37,7 +37,7 @@ namespace MUnique.OpenMU.GameServer.RemoteView.Character
             // 0.75 doesn't support dark lord (number 16) and newer classes yet
             var supportedCharacters = this.player.Account.Characters.Where(c => c.CharacterClass.Number < 16).OrderBy(c => c.CharacterSlot).ToList();
 
-            using var writer = this.player.Connection.StartSafeWrite(CharacterList075.Code, CharacterList075.GetRequiredSize(supportedCharacters.Count));
+            using var writer = this.player.Connection.StartSafeWrite(CharacterList075.HeaderType, CharacterList075.GetRequiredSize(supportedCharacters.Count));
             var packet = new CharacterList075(writer.Span)
             {
                 CharacterCount = (byte)supportedCharacters.Count,
