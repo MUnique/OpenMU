7f43a7b7b842b0e6500729c7f23c6d52cb2bf2d5
sven-n <sven-n@users.noreply.github.com>
2020-01-21 22:16:50 +0100

    Added plugins for initial items (Elf, MG)

---
+++ src/Persistence/Initialization/PlugIns/CharacterCreated/AddShortBowForFairyElf.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="AddShortBowForFairyElf.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Persistence.Initialization.PlugIns.CharacterCreated
+{
+    using System.Runtime.InteropServices;
+    using MUnique.OpenMU.Persistence.Initialization.CharacterClasses;
+    using MUnique.OpenMU.Persistence.Initialization.Items;
+    using MUnique.OpenMU.PlugIns;
+
+    /// <summary>
+    /// Adds a short bow to a created fairy elf character.
+    /// </summary>
+    [Guid("9EF17296-0436-4059-BC4E-0A71967F36EC")]
+    [PlugIn(nameof(AddShortBowForFairyElf), "Adds a short bow to a created fairy elf character.")]
+    public class AddShortBowForFairyElf : AddInitialItemPlugInBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddShortBowForFairyElf" /> class.
+        /// </summary>
+        public AddShortBowForFairyElf()
+            : base((byte)CharacterClassNumber.FairyElf, (byte)ItemGroups.Bows, 0, 1)
+        {
+        }
+    }
+}
--- src/Persistence/Initialization/PlugIns/CharacterCreated/AddSmallAxeForDarkKnight.cs
+++ src/Persistence/Initialization/PlugIns/CharacterCreated/AddSmallAxeForDarkKnight.cs
@@ -4,44 +4,24 @@
 
 namespace MUnique.OpenMU.Persistence.Initialization.PlugIns.CharacterCreated
 {
-    using System.Linq;
     using System.Runtime.InteropServices;
-    using MUnique.OpenMU.GameLogic;
-    using MUnique.OpenMU.GameLogic.PlugIns;
-    using MUnique.OpenMU.Persistence.BasicModel;
     using MUnique.OpenMU.Persistence.Initialization.CharacterClasses;
+    using MUnique.OpenMU.Persistence.Initialization.Items;
     using MUnique.OpenMU.PlugIns;
-    using Character = MUnique.OpenMU.DataModel.Entities.Character;
-    using Item = MUnique.OpenMU.DataModel.Entities.Item;
 
     /// <summary>
     /// Adds a small axe to a created dark knight character.
     /// </summary>
-    /// <seealso cref="MUnique.OpenMU.GameLogic.PlugIns.ICharacterCreatedPlugIn" />
     [Guid("2377C222-4418-4F17-8388-1F8825E6243C")]
     [PlugIn(nameof(AddSmallAxeForDarkKnight), "Adds a small axe to a created dark knight character.")]
-    public class AddSmallAxeForDarkKnight : ICharacterCreatedPlugIn
+    public class AddSmallAxeForDarkKnight : AddInitialItemPlugInBase
     {
-        /// <inheritdoc />
-        public void CharacterCreated(Player player, Character createdCharacter)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddSmallAxeForDarkKnight" /> class.
+        /// </summary>
+        public AddSmallAxeForDarkKnight() 
+            : base((byte)CharacterClassNumber.DarkKnight, (byte)ItemGroups.Axes, 0, 0)
         {
-            if (createdCharacter.CharacterClass.Number != (byte)CharacterClassNumber.DarkKnight
-                && createdCharacter.CharacterClass.Number != (byte)CharacterClassNumber.MagicGladiator)
-            {
-                return;
-            }
-
-            if (player.GameContext.Configuration.Items
-                    .FirstOrDefault(def => def.Group == 1 && def.Number == 0)
-                is { } smallAxeDefinition)
-            {
-                var smallAxe = player.PersistenceContext.CreateNew<Item>();
-
-                smallAxe.Definition = smallAxeDefinition;
-                smallAxe.Durability = smallAxe.Definition.Durability;
-                smallAxe.ItemSlot = 0;
-                createdCharacter.Inventory.Items.Add(smallAxe);
-            }
         }
     }
 }
