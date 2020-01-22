b0224c7cd4a9e4d2f5c47584e0a34b9318cfdec9
sven-n <sven-n@users.noreply.github.com>
2020-01-22 19:28:00 +0100

    Added initial skill 'Energy ball' for Dark Wizards
    See #146

---
+++ src/Persistence/Initialization/PlugIns/CharacterCreated/AddEnergyBallForDarkWizard.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="AddEnergyBallForDarkWizard.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Persistence.Initialization.PlugIns.CharacterCreated
+{
+    using System.Runtime.InteropServices;
+    using MUnique.OpenMU.Persistence.Initialization.CharacterClasses;
+    using MUnique.OpenMU.Persistence.Initialization.Skills;
+    using MUnique.OpenMU.PlugIns;
+
+    /// <summary>
+    /// Adds the energy ball skill to a created dark wizard character.
+    /// </summary>
+    [Guid("6A721FED-51CD-41AD-BA7C-EC1642FFF00A")]
+    [PlugIn(nameof(AddEnergyBallForDarkWizard), "Adds the energy ball skill to a created dark wizard character.")]
+    public class AddEnergyBallForDarkWizard : AddInitialSkillPlugInBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddEnergyBallForDarkWizard" /> class.
+        /// </summary>
+        public AddEnergyBallForDarkWizard()
+            : base((byte)CharacterClassNumber.DarkWizard, (ushort)SkillNumber.EnergyBall)
+        {
+        }
+    }
+}
