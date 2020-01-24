284e215700856b7ec6d219c9f8d436bac689c971
sven-n <sven-n@users.noreply.github.com>
2020-01-24 20:58:16 +0100

    Added initial skills for Rage Fighter, Summoner, Dark Lord
    Thanks @fos83  again!

---
+++ src/Persistence/Initialization/PlugIns/CharacterCreated/AddChargeForRageFighter.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="AddChargeForRageFighter.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Persistence.Initialization.PlugIns.CharacterCreated
+{
+    using System.Runtime.InteropServices;
+    using MUnique.OpenMU.Persistence.Initialization.CharacterClasses;
+    using MUnique.OpenMU.Persistence.Initialization.Skills;
+    using MUnique.OpenMU.PlugIns;
+
+    /// <summary>
+    /// Adds the Charge skill to a created rage fighter character.
+    /// </summary>
+    [Guid("53F23DDD-3676-4D24-8DFF-2EF657255832")]
+    [PlugIn(nameof(AddChargeForRageFighter), "Adds the Charge skill to a created rage fighter character.")]
+    public class AddChargeForRageFighter : AddInitialSkillPlugInBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddChargeForRageFighter"/> class.
+        /// </summary>
+        public AddChargeForRageFighter()
+            : base((byte)CharacterClassNumber.RageFighter, (ushort)SkillNumber.Charge)
+        {
+        }
+    }
+}
