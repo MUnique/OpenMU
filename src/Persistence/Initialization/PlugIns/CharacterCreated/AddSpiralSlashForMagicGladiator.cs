a7bc92c73ac7357b1c85a6d8e8ec869954a667e3
sven-n <sven-n@users.noreply.github.com>
2020-01-24 07:16:57 +0100

    Added initial castle siege skills for Dark Knight, Fairy Elf, Magic Gladiator
    Thanks @fos83

---
+++ src/Persistence/Initialization/PlugIns/CharacterCreated/AddSpiralSlashForMagicGladiator.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="AddSpiralSlashForMagicGladiator.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Persistence.Initialization.PlugIns.CharacterCreated
+{
+    using System.Runtime.InteropServices;
+    using MUnique.OpenMU.Persistence.Initialization.CharacterClasses;
+    using MUnique.OpenMU.Persistence.Initialization.Skills;
+    using MUnique.OpenMU.PlugIns;
+
+    /// <summary>
+    /// Adds the Spiral slash skill (active in castle siege) to a created magic gladiator character.
+    /// </summary>
+    [Guid("83B0D163-7E97-40FC-851A-D5500B4BB33E")]
+    [PlugIn(nameof(AddEnergyBallForDarkWizard), "Adds the Spiral slash skill (active in castle siege) to a created magic gladiator character.")]
+    public class AddSpiralSlashForMagicGladiator : AddInitialSkillPlugInBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddSpiralSlashForMagicGladiator"/> class.
+        /// </summary>
+        public AddSpiralSlashForMagicGladiator()
+            : base((byte)CharacterClassNumber.MagicGladiator, (ushort)SkillNumber.SpiralSlash)
+        {
+        }
+    }
+}
