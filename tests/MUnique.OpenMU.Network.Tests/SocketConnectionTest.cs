27a857e20e524df4a8bc0d7307acfedf710161e1
sven-n <sven-n@users.noreply.github.com>
2020-03-04 22:03:43 +0100

    Improved correctness of the Network project
    * Removed usages of obsolete OnWriterCompleted / OnReaderCompleted
    * Improved calls to PipeReader/PipeWriter.Complete when errors occur. They're more logical now.
    * Improved the Connection.Connected flag logic - it may now detect a disconnected connection more reliably.
    * Added exception handling when sending regeneration packets after a player disconnected.
    * Minor code style improvements

--- tests/MUnique.OpenMU.Network.Tests/SocketConnectionTest.cs
+++ tests/MUnique.OpenMU.Network.Tests/SocketConnectionTest.cs
@@ -8,6 +8,7 @@ namespace MUnique.OpenMU.Network.Tests
     using System.Net;
     using System.Net.Sockets;
     using System.Threading;
+    using System.Threading.Tasks;
     using NUnit.Framework;
     using Pipelines.Sockets.Unofficial;
 
@@ -81,6 +82,82 @@ namespace MUnique.OpenMU.Network.Tests
             }
 
             server.Stop();
+        }
+
+        /// <summary>
+        /// Tests if the connection is disconnected after sending invalid data.
+        /// </summary>
+        [Test]
+        public async Task TestDisconnectOnInvalidHeaderSent()
+        {
+            IConnection connection = null;
+            var server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            try
+            {
+                server.BeginAcceptSocket(
+                    asyncResult =>
+                    {
+                        var clientSocket = server.EndAcceptSocket(asyncResult);
+                        var socketConnection = SocketConnection.Create(clientSocket);
+                        connection = new Connection(socketConnection, new PipelinedDecryptor(socketConnection.Input), new PipelinedEncryptor(socketConnection.Output));
+                    }, null);
+
+                using var client = new TcpClient("127.0.0.1", 5000);
+                while (connection == null)
+                {
+                    Thread.Sleep(10);
+                }
+
+                connection.BeginReceive();
+
+                var packet = new byte[22222];
+                packet[0] = 0xDE;
+                packet[1] = 0xAD;
+                packet[2] = 0xBE;
+                packet[3] = 0xAF;
+                await connection.Output.WriteAsync(packet).ConfigureAwait(false);
+                await Task.Delay(1000);
+
+                Assert.That(connection.Connected, Is.False);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Tests if the connection is disconnected after receiving invalid data.
+        /// </summary>
+        [Test]
+        public void TestDisconnectOnInvalidHeaderReceived()
+        {
+            var server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            try
+            {
+                server.BeginAcceptSocket(
+                    asyncResult =>
+                    {
+                        var clientSocket = server.EndAcceptSocket(asyncResult);
+                        var packet = new byte[] { 0xDE, 0xAD, 0xBE, 0xEF, 0, 0, 0, 0, 0, 0 };
+                        clientSocket.BeginSend(packet, 0, packet.Length, SocketFlags.None, null, null);
+                    }, null);
+
+                using var client = new TcpClient("127.0.0.1", 5000);
+                var socketConnection = SocketConnection.Create(client.Client);
+                var connection = new Connection(socketConnection, new PipelinedDecryptor(socketConnection.Input), new PipelinedEncryptor(socketConnection.Output));
+
+                connection.BeginReceive();
+
+                Thread.Sleep(100);
+                Assert.That(connection.Connected, Is.False);
+            }
+            finally
+            {
+                server.Stop();
+            }
         }
     }
 }
