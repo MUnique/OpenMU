e0439ee17cc4aebbad857755e697bb14bb821ce8
Leandro Souza <leandrorlls@gmail.com>
2021-04-06 22:57:00 +0200

    Added disconnect chat command (GM)

---
+++ src/GameLogic/PlugIns/ChatCommands/Arguments/DisconnectChatCommandArgs.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="DisconnectChatCommandArgs.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.PlugIns.ChatCommands.Arguments
+{
+    /// <summary>
+    /// Arguments used by DisconnectChatCommandPlugIn.
+    /// </summary>
+    public class DisconnectChatCommandArgs : ArgumentsBase
+    {
+        /// <summary>
+        /// Gets or sets the character name.
+        /// </summary>
+        [Argument("characterName")]
+        public string? CharacterName { get; set; }
+    }
+}
