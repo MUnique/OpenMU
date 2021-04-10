1ba3b2f9fbe7e22f7e1fb9987dd56025e93cec48
Leandro Souza <leandrorlls@gmail.com>
2021-04-10 22:46:21 +0200

    Added online chat command (GM)

---
+++ src/GameLogic/PlugIns/ChatCommands/Arguments/EmptyChatCommandArgs.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="EmptyChatCommandArgs.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.PlugIns.ChatCommands.Arguments
+{
+    /// <summary>
+    /// Arguments used by any ChatCommandPlugIn which don't need any arguments.
+    /// </summary>
+    public class EmptyChatCommandArgs : ArgumentsBase
+    {
+    }
+}
