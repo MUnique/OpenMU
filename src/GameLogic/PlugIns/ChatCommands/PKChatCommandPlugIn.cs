2feeddfc9fffddf5f5ec20fb42d0e307f2446f79
Leandro Souza <leandrorlls@gmail.com>
2021-04-12 22:09:49 +0200

    Updated pk chat command (GM)

--- src/GameLogic/PlugIns/ChatCommands/PKChatCommandPlugIn.cs
+++ src/GameLogic/PlugIns/ChatCommands/PKChatCommandPlugIn.cs
@@ -8,6 +8,7 @@ namespace MUnique.OpenMU.GameLogic.PlugIns.ChatCommands.GameMaster
     using System.Runtime.InteropServices;
     using MUnique.OpenMU.DataModel.Entities;
     using MUnique.OpenMU.GameLogic.PlugIns.ChatCommands.Arguments;
+    using MUnique.OpenMU.GameLogic.Views.Character;
     using MUnique.OpenMU.PlugIns;
 
     /// <summary>
@@ -15,11 +16,13 @@ namespace MUnique.OpenMU.GameLogic.PlugIns.ChatCommands.GameMaster
     /// </summary>
     /// <seealso cref="MUnique.OpenMU.GameLogic.PlugIns.ChatCommands.IChatCommandPlugIn" />
     [Guid("30B7EFF0-33EE-4136-BEB0-BE503B748DC6")]
-    [PlugIn("PK chat command", "Handles the chat command '/pk <characterName>'. Increments the character murders.")]
+    [PlugIn("PK chat command", "Handles the chat command '/pk <character> <pk_level> <pk_count>'. Sets PK Level and Count for a character.")]
     [ChatCommandHelp(Command, typeof(PKChatCommandArgs), CharacterStatus.GameMaster)]
     public class PKChatCommandPlugIn : ChatCommandPlugInBase<PKChatCommandArgs>
     {
         private const string Command = "/pk";
+        private const int MinPkLevel = HeroState.PlayerKillWarning - HeroState.Normal;
+        private const int MaxPkLevel = HeroState.PlayerKiller2ndStage - HeroState.Normal;
 
         /// <inheritdoc />
         public override string Key => Command;
@@ -30,8 +33,21 @@ namespace MUnique.OpenMU.GameLogic.PlugIns.ChatCommands.GameMaster
         /// <inheritdoc />
         protected override void DoHandleCommand(Player gameMaster, PKChatCommandArgs arguments)
         {
+            if (arguments.Level < MinPkLevel || arguments.Level > MaxPkLevel)
+            {
+                throw new ArgumentException($"PK level must be between {MinPkLevel} and {MaxPkLevel}.");
+            }
+
+            if (arguments.Count <= default(int))
+            {
+                throw new ArgumentException($"PK count must be greater than zero.");
+            }
+
             var targetPlayer = this.GetPlayerByCharacterName(gameMaster, arguments.CharacterName ?? string.Empty);
-            targetPlayer.AfterKilledPlayer();
+            targetPlayer.SelectedCharacter!.State = HeroState.Normal + arguments.Level;
+            targetPlayer.SelectedCharacter!.StateRemainingSeconds = (int)TimeSpan.FromHours(arguments.Count).TotalSeconds;
+            targetPlayer.SelectedCharacter!.PlayerKillCount = arguments.Count;
+            targetPlayer.ForEachWorldObserver(o => o.ViewPlugIns.GetPlugIn<IUpdateCharacterHeroStatePlugIn>()?.UpdateCharacterHeroState(targetPlayer), true);
 
             var message = string.Format(
                 "The state of {0} has been changed to {1} with {2} murders for {3} minutes",
