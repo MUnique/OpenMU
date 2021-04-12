8a821e666c6737e8010badd3e331d9fa56c4f363
Leandro Souza <leandrorlls@gmail.com>
2021-04-13 00:49:13 +0200

    Added guild disconnect chat command (GM)

---
+++ src/GameLogic/PlugIns/ChatCommands/Arguments/GuildDisconnectChatCommandArgs.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="GuildDisconnectChatCommandArgs.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.PlugIns.ChatCommands.Arguments
+{
+    /// <summary>
+    /// Arguments used by GuildDisconnectChatCommandPlugIn.
+    /// </summary>
+    public class GuildDisconnectChatCommandArgs : ArgumentsBase
+    {
+        /// <summary>
+        /// Gets or sets the guild name.
+        /// </summary>
+        [Argument("guildName")]
+        public string? GuildName { get; set; }
+    }
+}
