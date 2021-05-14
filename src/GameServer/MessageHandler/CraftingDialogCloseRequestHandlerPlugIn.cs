0c12b70f2bde0409122281c62d49da038559eb12
sven-n <sven-n@users.noreply.github.com>
2021-05-14 19:04:10 +0200

    Fixed file name

--- src/GameServer/MessageHandler/CraftingDialogCloseRequestHandlerPlugIn.cs
+++ src/GameServer/MessageHandler/CraftingDialogCloseRequestHandlerPlugIn.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="ChaosMachineCloseHandlerPlugIn.cs" company="MUnique">
+﻿// <copyright file="CraftingDialogCloseRequestHandlerPlugIn.cs" company="MUnique">
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
