9664e240eb68e9cb4712a5ce51f9bbf7ec687529
sven-n <sven-n@users.noreply.github.com>
2021-05-25 18:14:25 +0200

    View plugin interfaces for guild wars

---
+++ src/GameLogic/Views/Guild/GuildWarResult.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="GuildWarResult.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.Views.Guild
+{
+    /// <summary>
+    /// The result of a guild war.
+    /// </summary>
+    public enum GuildWarResult
+    {
+        /// <summary>
+        /// The guild war was lost.
+        /// </summary>
+        Lost,
+
+        /// <summary>
+        /// The guild war was won.
+        /// </summary>
+        Won,
+
+        /// <summary>
+        /// The other guild master cancelled the guild war.
+        /// </summary>
+        OtherGuildMasterCancelledWar,
+
+        /// <summary>
+        /// The own guild master cancelled the guild war.
+        /// </summary>
+        CancelledWar,
+    }
+}
---
+++ src/GameLogic/Views/Guild/IShowGuildWarRequestPlugIn.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IShowGuildWarRequestPlugIn.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.Views.Guild
+{
+    using MUnique.OpenMU.GameLogic.GuildWar;
+
+    /// <summary>
+    /// Interface of a view whose implementation informs about a guild war request of another guild master.
+    /// </summary>
+    public interface IShowGuildWarRequestPlugIn : IViewPlugIn
+    {
+        /// <summary>
+        /// Shows the guild war request.
+        /// </summary>
+        /// <param name="requestingGuildName">Name of the requesting guild.</param>
+        /// <param name="warType">Type of the war.</param>
+        void ShowRequest(string requestingGuildName, GuildWarType warType);
+    }
+}
