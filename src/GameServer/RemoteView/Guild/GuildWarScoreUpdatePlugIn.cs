61038af8f5f31ad06dc6c7a6254992c2ff70ac1c
sven-n <sven-n@users.noreply.github.com>
2021-05-25 18:23:20 +0200

    The view plugins for guild packets

---
+++ src/GameServer/RemoteView/Guild/GuildWarScoreUpdatePlugIn.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="GuildWarScoreUpdatePlugIn.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameServer.RemoteView.Guild
+{
+    using System.Runtime.InteropServices;
+    using MUnique.OpenMU.GameLogic.Views.Guild;
+    using MUnique.OpenMU.Network.Packets.ServerToClient;
+    using MUnique.OpenMU.PlugIns;
+
+    /// <summary>
+    /// The default implementation of the <see cref="IGuildWarScoreUpdatePlugIn"/> which is forwarding everything to the game client with specific data packets.
+    /// </summary>
+    [PlugIn(nameof(IGuildWarScoreUpdatePlugIn), "The default implementation of the IGuildWarScoreUpdatePlugIn which is forwarding everything to the game client with specific data packets.")]
+    [Guid("24E5E0C1-DAE7-4E34-810D-C622F8F9B70F")]
+    public class GuildWarScoreUpdatePlugIn : IGuildWarScoreUpdatePlugIn
+    {
+        private readonly RemotePlayer player;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GuildWarScoreUpdatePlugIn"/> class.
+        /// </summary>
+        /// <param name="player">The player.</param>
+        public GuildWarScoreUpdatePlugIn(RemotePlayer player) => this.player = player;
+
+        /// <inheritdoc/>
+        public void UpdateScore()
+        {
+            if (this.player.GuildWarContext is { } guildWarContext)
+            {
+                this.player.Connection?.SendGuildWarScoreUpdate(guildWarContext.ThisScore, guildWarContext.EnemyScore);
+            }
+        }
+    }
+}
---
+++ src/GameServer/RemoteView/Guild/ShowGuildWarResultPlugIn.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="ShowGuildWarResultPlugIn.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameServer.RemoteView.Guild
+{
+    using System.Runtime.InteropServices;
+    using MUnique.OpenMU.GameLogic.Views.Guild;
+    using MUnique.OpenMU.Network.Packets.ServerToClient;
+    using MUnique.OpenMU.PlugIns;
+
+    /// <summary>
+    /// The default implementation of the <see cref="IShowGuildWarResultPlugIn"/> which is forwarding everything to the game client with specific data packets.
+    /// </summary>
+    [PlugIn(nameof(ShowGuildWarResultPlugIn), "The default implementation of the IShowGuildWarResultPlugIn which is forwarding everything to the game client with specific data packets.")]
+    [Guid("991247B9-D4A3-466D-A7CE-2621843CA94F")]
+    public class ShowGuildWarResultPlugIn: IShowGuildWarResultPlugIn
+    {
+        private readonly RemotePlayer player;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShowGuildWarResultPlugIn"/> class.
+        /// </summary>
+        /// <param name="player">The player.</param>
+        public ShowGuildWarResultPlugIn(RemotePlayer player) => this.player = player;
+
+        /// <inheritdoc/>
+        public void ShowResult(string hostileGuildName, GuildWarResult result)
+        {
+            this.player.Connection?.SendGuildWarEnded(result.Convert(), hostileGuildName);
+        }
+    }
+}
