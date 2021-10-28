9f6b1a1db4848988e350a44ae1bbad133a4ebe0e
sven-n <sven-n@users.noreply.github.com>
2021-10-28 20:48:18 +0200

    fixed namespace

--- src/GameLogic/PlayerActions/Skills/AreaSkillAttackAction.cs
+++ src/GameLogic/PlayerActions/Skills/AreaSkillAttackAction.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
-namespace MUnique.OpenMU.GameLogic.PlayerActions
+namespace MUnique.OpenMU.GameLogic.PlayerActions.Skills
 {
     using System.Linq;
     using MUnique.OpenMU.DataModel;
