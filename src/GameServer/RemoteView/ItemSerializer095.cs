7f7091a65cc912edcaac77e80e50720017129e91
sven-n <sven-n@users.noreply.github.com>
2021-11-05 18:53:05 +0100

    Fixed item serialization

--- src/GameServer/RemoteView/ItemSerializer095.cs
+++ src/GameServer/RemoteView/ItemSerializer095.cs
@@ -16,11 +16,27 @@ namespace MUnique.OpenMU.GameServer.RemoteView
     using MUnique.OpenMU.PlugIns;
 
     /// <summary>
-    /// This item serializer is used to serialize the item data to the data packets for version 0.75.
-    /// Each item is serialized into a 3-byte long part of an array.
+    /// This item serializer is used to serialize the item data to the data packets for version 0.95.
+    /// Each item is serialized into a 4-byte long part of an array.
     /// </summary>
+    /// <remarks>
+    /// Bit pattern:
+    /// Index   Bits
+    /// 0       gggn nnnn
+    /// 1       slll lLoo
+    /// 2       Durability
+    /// 3       goxx xxxx
+    ///
+    /// g = group
+    /// n = number
+    /// l = level
+    /// L = luck
+    /// s = skill
+    /// o = option level
+    /// x = exc option flags.
+    /// </remarks>
     [Guid("4BD85C02-C43E-494D-B6B8-767ED94E09F0")]
-    [PlugIn(nameof(ItemSerializer095), "The item serializer for game client version 0.75")]
+    [PlugIn(nameof(ItemSerializer095), "The item serializer for game client version 0.95")]
     [MinimumClient(0, 95, ClientLanguage.Invariant)]
     public class ItemSerializer095 : IItemSerializer
     {
@@ -37,8 +53,8 @@ namespace MUnique.OpenMU.GameServer.RemoteView
         public void SerializeItem(Span<byte> target, Item item)
         {
             item.ThrowNotInitializedProperty(item.Definition is null, nameof(item.Definition));
-            
-            var itemType = (item.Definition.Number & 0x1F) + (item.Definition.Group << 5);
+
+            var itemType = (item.Definition.Number & 0x1F) | (item.Definition.Group << 5);
             target[0] = (byte)(itemType & 0xFF);
             if (itemType > 0xFF)
             {
@@ -51,16 +67,10 @@ namespace MUnique.OpenMU.GameServer.RemoteView
             if (itemOption != null)
             {
                 target[1] |= (byte)(itemOption.Level & 3);
-                target[3] = (byte)((itemOption.Level & 4) << 4); // The highest bit is placed into the 2nd bit of the exc byte (0x40).
-
-                // Some items (wings) can have different options (3rd wings up to 3!)
-                // Alternate options are set at array[startIndex + 3] |= 0x20 and 0x10
-                // TODO: FInd out if we have 2nd wings
-                if (itemOption.ItemOption?.Number > 0)
-                {
-                    target[3] |= (byte)((itemOption.ItemOption.Number & 0b11) << 4);
-                }
+                target[3] |= (byte)((itemOption.Level & 4) << 4); // The highest bit is placed into the 2nd bit of the exc byte (0x40).
             }
+
+            target[3] |= GetExcellentByte(item);
 
             if (item.ItemOptions.Any(o => o.ItemOption?.OptionType == ItemOptionTypes.Luck))
             {
@@ -79,6 +89,19 @@ namespace MUnique.OpenMU.GameServer.RemoteView
         public Item DeserializeItem(Span<byte> array, GameConfiguration gameConfiguration, IContext persistenceContext)
         {
             throw new NotImplementedException();
+        }
+
+        private static byte GetExcellentByte(Item item)
+        {
+            byte result = 0;
+            var excellentOptions = item.ItemOptions.Where(o => o.ItemOption?.OptionType == ItemOptionTypes.Excellent);
+
+            foreach (var option in excellentOptions)
+            {
+                result |= (byte)(1 << (option.ItemOption!.Number - 1));
+            }
+
+            return result;
         }
     }
 }
