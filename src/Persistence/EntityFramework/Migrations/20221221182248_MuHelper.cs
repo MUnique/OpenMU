5cadb3cd26a30632c4e33e63b79024c2ff59b1bc
sven-n <sven-n@users.noreply.github.com>
2022-12-21 19:23:21 +0100

    Added database migration
    Added Character.MuHelperConfiguration

---
+++ src/Persistence/EntityFramework/Migrations/20221221182248_MuHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MUnique.OpenMU.Persistence.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class MuHelper : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "MuHelperConfiguration",
+                schema: "data",
+                table: "Character",
+                type: "bytea",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MuHelperConfiguration",
+                schema: "data",
+                table: "Character");
+        }
+    }
+}
