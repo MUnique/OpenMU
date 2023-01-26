3ec790c601373f6ce2184c51fa764a0d5269aaad
Marcos De Noni Buturri <mdbuturri@gmail.com>
2023-01-26 17:20:23 -0300

    Added /charinfo chat command plugin

--- src/DataModel/Entities/Character.cs
+++ src/DataModel/Entities/Character.cs
@@ -55,17 +55,17 @@ public enum HeroState
 public enum CharacterStatus
 {
     /// <summary>
-    /// The character is normal
+    /// The character is normal.
     /// </summary>
     Normal = 0,
 
     /// <summary>
-    /// The character is banned
+    /// The character is banned.
     /// </summary>
     Banned = 1,
 
     /// <summary>
-    /// The character is a GameMaster (have mu logo on the head)
+    /// The character is a GameMaster (have mu logo on the head).
     /// </summary>
     GameMaster = 32,
 }
@@ -252,6 +252,32 @@ public class Character
     /// <inheritdoc />
     public override string ToString()
     {
-        return this.Name;
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine($"Id: {this.Id}");
+        sb.AppendLine($"Name: {this.Name}");
+        sb.AppendLine($"Class: {this.CharacterClass?.Name}");
+        sb.AppendLine($"Slot: {this.CharacterSlot}");
+        sb.AppendLine($"Create Date: {this.CreateDate}");
+        sb.AppendLine($"Exp: {this.Experience}");
+        sb.AppendLine($"Level Up Points: {this.LevelUpPoints}");
+        sb.AppendLine($"Master Exp: {this.MasterExperience}");
+        sb.AppendLine($"Master Lv Up Points: {this.MasterLevelUpPoints}");
+        sb.AppendLine($"Location: {this.CurrentMap?.Name}({this.PositionX}, {this.PositionY})");
+        sb.AppendLine($"Kill Count: {this.PlayerKillCount}");
+        sb.AppendLine($"State Remaining Seconds: {this.StateRemainingSeconds}");
+        sb.AppendLine($"State: {Enum.GetName(this.State)}");
+        sb.AppendLine($"Status: {Enum.GetName(this.CharacterStatus)}");
+        sb.AppendLine($"Used Fruit Points: {this.UsedFruitPoints}");
+        sb.AppendLine($"Used Neg Fruit Points: {this.UsedNegFruitPoints}");
+        sb.AppendLine($"Inventory Extensions: {this.InventoryExtensions}");
+
+        foreach (var attribute in this.Attributes)
+        {
+            sb.AppendLine($"Att( {attribute.Definition?.Designation} ): {attribute.Value}");
+        }
+
+        // return this.Name;
+        return sb.ToString();
     }
 }
---
+++ src/GameLogic/PlugIns/ChatCommands/CharInfoChatCommandPlugIn.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="CharInfoChatCommandPlugIn.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.PlugIns.ChatCommands;
+
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using MUnique.OpenMU.DataModel.Entities;
+using MUnique.OpenMU.GameLogic.PlugIns.ChatCommands.Arguments;
+using MUnique.OpenMU.PlugIns;
+
+/// <summary>
+/// A chat command plugin which handles charinfo commands.
+/// </summary>
+[Guid("0C7162BC-C74E-4A65-82E3-12811E4BE170")]
+[PlugIn("Char Info command", "Handles the chat command '/charinfo <char>'. Returns information about the character back to the requester.")]
+[ChatCommandHelp(Command, "Returns information about the character back to the requester.", typeof(CharInfoChatCommandArgs), CharacterStatus.GameMaster)]
+public class CharInfoChatCommandPlugIn : ChatCommandPlugInBase<CharInfoChatCommandArgs>
+{
+    private const string Command = "/charinfo";
+
+    /// <inheritdoc />
+    public override string Key => Command;
+
+    /// <inheritdoc/>
+    public override CharacterStatus MinCharacterStatusRequirement => CharacterStatus.GameMaster;
+
+    /// <inheritdoc />
+    protected override async ValueTask DoHandleCommandAsync(Player gameMaster, CharInfoChatCommandArgs arguments)
+    {
+        if (string.IsNullOrEmpty(arguments.CharacterName))
+        {
+            throw new ArgumentException($"{nameof(arguments.CharacterName)} is required.");
+        }
+
+        string characterName = arguments.CharacterName ?? string.Empty;
+
+        using var context = gameMaster.GameContext.PersistenceContextProvider.CreateNewPlayerContext(gameMaster.GameContext.Configuration);
+        var account = await context.GetAccountByCharacterNameAsync(characterName).ConfigureAwait(false);
+
+        if (account == null)
+        {
+            throw new ArgumentException($"Character account not found.");
+        }
+
+        await this.ShowMessageToAsync(gameMaster, $"Account Name: {account.LoginName}").ConfigureAwait(false);
+
+        var character = account.Characters.First(c => c.Name == characterName);
+
+        using var reader = new StringReader(character.ToString());
+
+        while (true)
+        {
+            var line = await reader.ReadLineAsync().ConfigureAwait(false);
+            if (line == null)
+            {
+                break;
+            }
+
+            await this.ShowMessageToAsync(gameMaster, line).ConfigureAwait(false);
+        }
+    }
+}
