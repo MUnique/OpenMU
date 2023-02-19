ebb63c18a73a91e38c2f7275c5dacf01f5d9bee1
sven-n <sven-n@users.noreply.github.com>
2023-02-19 11:11:13 +0100

    Added tool to decrypt MuError.log

---
+++ src/ClientErrorLogDecryptor/Program.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="Program.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.IO;
+
+var logPath = args.FirstOrDefault();
+if (string.IsNullOrWhiteSpace(logPath))
+{
+    Console.WriteLine("No path to the error log specified as first parameter. Press any key to exit...");
+    Console.ReadKey();
+    return;
+}
+
+var decryptionKey = new byte[] { 0x7C, 0xBD, 0x81, 0x9F, 0x3D, 0x93, 0xE2, 0x56, 0x2A, 0x73, 0xD2, 0x3E, 0xF2, 0x83, 0x95, 0xBF };
+try
+{
+    var fileBytes = await File.ReadAllBytesAsync(logPath).ConfigureAwait(false);
+
+    var resultBuilder = new StringBuilder();
+    var i = 0;
+    foreach (var character in fileBytes)
+    {
+        
+        var result = (char)(character ^ decryptionKey[i % decryptionKey.Length]);
+        resultBuilder.Append(result);
+        i++;
+    }
+
+    var resultPath = logPath + ".decrypted.txt";
+    File.WriteAllText(resultPath, resultBuilder.ToString());
+    Console.WriteLine($"Decrypted file has been written to {resultPath}. Press any key to exit...");
+    
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Unexpected error during decrypting the file '{logPath}':");
+    Console.WriteLine(ex);
+
+    Console.WriteLine($"Press any key to exit...");
+}
+
+Console.ReadKey();
---
+++ src/ClientErrorLogDecryptor/Readme.md
@@ -0,0 +1,17 @@
+﻿# Client Error Log Decryptor
+
+This tool can be used to decrypt the MuError.log files which are created by the
+game client on every start.
+
+It might help to find/analyse errors which occured on the client side.
+
+## Usage
+
+This is a command line tool. The file name is given as first parameter. If it
+contains spaces, wrap it by quotes.
+Example: ```MUnique.OpenMU.ClientErrorLogDecryptor.exe "C:\MU-Season 6E3\MuError.log"```
+
+On windows you can also just simply drag & drop your MuError.log file on the exe.
+
+The resulting file is written out into a new file, where the file name is extended
+by ".decrypted.txt".
