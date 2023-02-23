3a7dfa4a048d78eaa15090d4279b080421ba4bac
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-23 11:44:32 -0300

    doc(ItemExtensions): Add summary.

--- src/Persistence/EntityFramework/Extensions/ModelBuilder/ItemExtensions.cs
+++ src/Persistence/EntityFramework/Extensions/ModelBuilder/ItemExtensions.cs
@@ -5,11 +5,19 @@ using MUnique.OpenMU.Persistence.EntityFramework.Model;
 
 internal static class ItemExtensions
 {
+    /// <summary>
+    /// Extension for ItemStorage entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<ItemStorage> builder)
     {
         builder.HasMany(storage => storage.RawItems).WithOne(item => item.RawItemStorage!);
     }
 
+    /// <summary>
+    /// Extension for ItemSetGroup entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<ItemSetGroup> builder)
     {
         builder.HasMany(isg => isg.RawItems).WithOne(item => item.RawItemSetGroup!);
