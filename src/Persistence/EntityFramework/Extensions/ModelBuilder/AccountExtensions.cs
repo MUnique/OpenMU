ce59d923bee487c34489e07b84e9f7012ba6cbd6
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-23 11:43:00 -0300

    doc(AccountExtensions): Add summary.

--- src/Persistence/EntityFramework/Extensions/ModelBuilder/AccountExtensions.cs
+++ src/Persistence/EntityFramework/Extensions/ModelBuilder/AccountExtensions.cs
@@ -5,6 +5,10 @@ using MUnique.OpenMU.Persistence.EntityFramework.Model;
 
 internal static class AccountExtensions
 {
+    /// <summary>
+    /// Extension for Account entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<Account> builder)
     {
         builder.Property(account => account.LoginName).HasMaxLength(10).IsRequired();
