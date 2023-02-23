6083d61b19d581106a5a73c02bcce37bd32966be
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-23 11:43:38 -0300

    doc(DefinitionExtensions): Add summary.

--- src/Persistence/EntityFramework/Extensions/ModelBuilder/DefinitionExtensions.cs
+++ src/Persistence/EntityFramework/Extensions/ModelBuilder/DefinitionExtensions.cs
@@ -5,21 +5,37 @@ using MUnique.OpenMU.Persistence.EntityFramework.Model;
 
 internal static class DefinitionExtensions
 {
+    /// <summary>
+    /// Extension for MonsterDefinition entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<MonsterDefinition> builder)
     {
         builder.HasMany<QuestDefinition>().WithOne(q => q.RawQuestGiver);
     }
 
+    /// <summary>
+    /// Extension for ItemBasePowerUpDefinition entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<ItemBasePowerUpDefinition> builder)
     {
         builder.Ignore(d => d.BaseValueElement);
     }
 
+    /// <summary>
+    /// Extension for PowerUpDefinitionValue entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<PowerUpDefinitionValue> builder)
     {
         builder.Ignore(p => p.ConstantValue);
     }
 
+    /// <summary>
+    /// Extension for MasterSkillDefinition entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<MasterSkillDefinition> builder)
     {
         builder.HasOne(s => s.RawRoot);
