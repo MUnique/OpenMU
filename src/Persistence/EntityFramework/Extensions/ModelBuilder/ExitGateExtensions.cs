04e88ff715a488b868b8673eb859c5fce47085aa
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-23 11:43:51 -0300

    doc(ExitGate): Add summary.

--- src/Persistence/EntityFramework/Extensions/ModelBuilder/ExitGateExtensions.cs
+++ src/Persistence/EntityFramework/Extensions/ModelBuilder/ExitGateExtensions.cs
@@ -5,6 +5,10 @@ using MUnique.OpenMU.Persistence.EntityFramework.Model;
 
 internal static class ExitGateExtensions
 {
+    /// <summary>
+    /// Extension for ExitGate entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<ExitGate> builder)
     {
         builder.HasOne(gate => gate.RawMap);
