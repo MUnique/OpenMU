c42cab9ba1db61affc3e513bc039222b6ec2723e
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-23 11:45:02 -0300

    doc(LevelExtensions): Add summary.

--- src/Persistence/EntityFramework/Extensions/ModelBuilder/LevelBonusExtensions.cs
+++ src/Persistence/EntityFramework/Extensions/ModelBuilder/LevelBonusExtensions.cs
@@ -5,6 +5,10 @@ using MUnique.OpenMU.Persistence.EntityFramework.Model;
 
 internal static class LevelBonusExtensions
 {
+    /// <summary>
+    /// Extension for LevelBonus entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<LevelBonus> builder)
     {
         builder.Ignore(l => l.AdditionalValueElement);
