439aed558c0a5dcb5e5ee262e8ce4e5ab314d8ee
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-23 11:44:52 -0300

    doc(LetterExtensions): Add summary.

--- src/Persistence/EntityFramework/Extensions/ModelBuilder/LetterExtensions.cs
+++ src/Persistence/EntityFramework/Extensions/ModelBuilder/LetterExtensions.cs
@@ -5,11 +5,19 @@ using MUnique.OpenMU.Persistence.EntityFramework.Model;
 
 internal static class LetterExtensions
 {
+    /// <summary>
+    /// Extension for LetterBody entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<LetterBody> builder)
     {
         builder.HasOne(body => body.RawHeader);
     }
 
+    /// <summary>
+    /// Extension for LetterHeader entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<LetterHeader> builder)
     {
         builder.Ignore(header => header.ReceiverName);
