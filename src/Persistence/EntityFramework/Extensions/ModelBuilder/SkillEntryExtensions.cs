63a93373b048d6149e06405ddeda4aa260c1e966
Marcus.Silva <marcus.silva@xpi.com.br>
2023-02-23 11:45:54 -0300

    doc(SkillEntryExtensions): Add summary.

--- src/Persistence/EntityFramework/Extensions/ModelBuilder/SkillEntryExtensions.cs
+++ src/Persistence/EntityFramework/Extensions/ModelBuilder/SkillEntryExtensions.cs
@@ -5,6 +5,10 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 internal static class SkillEntryExtensions
 {
+    /// <summary>
+    /// Extension for SkillEntry entity.
+    /// centralize logic for apply configuration on database with code first.
+    /// </summary>
     public static void Apply(this EntityTypeBuilder<SkillEntry> builder)
     {
         builder.Ignore(s => s.PowerUps);
