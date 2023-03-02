147abc15fc9ce43cd8552a9f2f866cc9ac238ecd
sven-n <sven-n@users.noreply.github.com>
2023-03-02 21:02:02 +0100

    First draft of an update system
    Updates can simply be implemented as IConfigurationUpdatePlugIn.

---
+++ src/Persistence/Initialization/Updates/DataUpdateManager.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="DataUpdateManager.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Persistence.Initialization.Updates;
+
+using MUnique.OpenMU.DataModel.Configuration;
+using MUnique.OpenMU.PlugIns;
+
+/// <summary>
+/// Manager which applies updates of previously initialized data by a <see cref="IDataInitializationPlugIn"/>.
+/// </summary>
+internal class DataUpdateManager
+{
+    private readonly IPersistenceContextProvider _contextProvider;
+    private readonly PlugInManager _plugInManager;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataUpdateManager"/> class.
+    /// </summary>
+    /// <param name="contextProvider">The context provider.</param>
+    /// <param name="plugInManager">The plug in manager.</param>
+    public DataUpdateManager(IPersistenceContextProvider contextProvider, PlugInManager plugInManager)
+    {
+        this._contextProvider = contextProvider;
+        this._plugInManager = plugInManager;
+    }
+
+    /// <summary>
+    /// Determines the available updates.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="System.InvalidOperationException">The plugin manager is not initialized.</exception>
+    public async ValueTask<IReadOnlyCollection<IConfigurationUpdatePlugIn>> DetermineAvailableUpdatesAsync(IDataInitializationPlugIn initializationPlugIn)
+    {
+        using var context = this._contextProvider.CreateNewContext();
+        var updates = (await context.GetAsync<ConfigurationUpdate>().ConfigureAwait(false)).ToList();
+        var maxVersion = updates.Where(up => up.InstalledAt is not null).Max(up => up.Version);
+        var notInstalledUpdates = updates
+            .Where(up => up.InstalledAt is null)
+            .Select(up => up.Version)
+            .ToHashSet();
+
+        var updateStrategyProvider = this._plugInManager.GetStrategyProvider<int, IConfigurationUpdatePlugIn>()
+                                     ?? throw new InvalidOperationException("The plugin manager is not initialized.");
+
+        var availableUpdates = updateStrategyProvider.AvailableStrategies
+            .Where(up => up.DataInitializationKey == initializationPlugIn.Key)
+            .Where(up => up.Version > maxVersion || notInstalledUpdates.Contains(up.Version))
+            .OrderBy(up => up.Version)
+            .ToList();
+
+        return availableUpdates;
+    }
+
+    public async ValueTask ApplyUpdatesAsync(IEnumerable<IConfigurationUpdatePlugIn> updates)
+    {
+        using var context = this._contextProvider.CreateNewContext();
+        foreach (var update in updates)
+        {
+            await update.ApplyUpdateAsync(context).ConfigureAwait(false);
+            await context.SaveChangesAsync().ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/Persistence/Initialization/Updates/UpdatePlugInBase.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="UpdatePlugInBase.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using MUnique.OpenMU.DataModel.Configuration;
+
+namespace MUnique.OpenMU.Persistence.Initialization.Updates;
+
+/// <summary>
+/// Abstract base class for a <see cref="IConfigurationUpdatePlugIn"/>.
+/// </summary>
+public abstract class UpdatePlugInBase : IConfigurationUpdatePlugIn
+{
+    /// <inheritdoc />
+    public int Key => this.Version;
+
+    /// <inheritdoc />
+    public abstract int Version { get; }
+
+    /// <inheritdoc />
+    public abstract string DataInitializationKey { get; }
+
+    /// <inheritdoc />
+    public abstract string Name { get; }
+
+    /// <inheritdoc />
+    public abstract string Description { get; }
+
+    /// <inheritdoc />
+    public abstract DateTime CreatedAt { get; }
+
+    /// <inheritdoc />
+    public abstract bool IsMandatory { get; }
+
+    /// <inheritdoc />
+    public async ValueTask ApplyUpdateAsync(IContext context)
+    {
+        await this.ApplyAsync(context);
+        this.AddUpdateEntry(context);
+    }
+
+    /// <summary>
+    /// Applies this update on the given persistence context.
+    /// </summary>
+    /// <param name="context">The persistence context.</param>
+    /// <remarks>
+    /// Calling <see cref="IContext.SaveChangesAsync"/> is not required in this implementation.
+    /// It will be called by <see cref="DataUpdateManager"/>.
+    /// </remarks>
+    protected abstract ValueTask ApplyAsync(IContext context);
+
+    private void AddUpdateEntry(IContext context)
+    {
+        var entry = context.CreateNew<ConfigurationUpdate>();
+        entry.Version = this.Version;
+        entry.Name = this.Name;
+        entry.Description = this.Description;
+        entry.CreatedAt = this.CreatedAt;
+        entry.InstalledAt = DateTime.UtcNow;
+    }
+}
