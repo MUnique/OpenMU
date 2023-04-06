c3e7eb87e41b3c52543d3cea6a6d0196bd9b85fd
sven-n <sven-n@users.noreply.github.com>
2023-04-06 22:00:28 +0200

    Updated merchants page
    It uses the item storage editor now as well, so it's much easier to edit the items now.

--- src/Web/AdminPanel/Pages/Merchants.razor.cs
+++ src/Web/AdminPanel/Pages/Merchants.razor.cs
@@ -6,14 +6,14 @@ namespace MUnique.OpenMU.Web.AdminPanel.Pages;
 
 using System.ComponentModel;
 using System.Threading;
-using Blazored.Modal;
 using Blazored.Modal.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.QuickGrid;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
 using MUnique.OpenMU.DataModel.Configuration;
 using MUnique.OpenMU.DataModel.Entities;
 using MUnique.OpenMU.Persistence;
-using MUnique.OpenMU.Web.AdminPanel.Components.Form;
 
 /// <summary>
 /// Razor page which shows objects of the specified type in a grid.
@@ -27,6 +27,8 @@ public partial class Merchants : ComponentBase, IAsyncDisposable
     private CancellationTokenSource? _disposeCts;
     private List<MerchantStorageViewModel>? _viewModels;
     private MerchantStorageViewModel? _selectedMerchant;
+    private IContext? _persistenceContext;
+    private IDisposable? _navigationLockDisposable;
 
     /// <summary>
     /// Gets or sets the data source.
@@ -52,11 +54,20 @@ public partial class Merchants : ComponentBase, IAsyncDisposable
     [Inject]
     public NavigationManager NavigationManager { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets the java script runtime.
+    /// </summary>
+    [Inject]
+    public IJSRuntime JavaScript { get; set; } = null!;
+
     private IQueryable<MerchantStorageViewModel>? ViewModels => this._viewModels?.AsQueryable();
 
     /// <inheritdoc />
     public async ValueTask DisposeAsync()
     {
+        this._navigationLockDisposable?.Dispose();
+        this._navigationLockDisposable = null;
+
         this._disposeCts?.Cancel();
         this._disposeCts?.Dispose();
         this._disposeCts = null;
@@ -76,6 +87,13 @@ public partial class Merchants : ComponentBase, IAsyncDisposable
     }
 
     /// <inheritdoc />
+    protected override Task OnInitializedAsync()
+    {
+        this._navigationLockDisposable = this.NavigationManager.RegisterLocationChangingHandler(this.OnBeforeInternalNavigationAsync);
+        return base.OnInitializedAsync();
+    }
+
+    /// <inheritdoc />
     protected override async Task OnParametersSetAsync()
     {
         var cts = new CancellationTokenSource();
@@ -84,10 +102,33 @@ public partial class Merchants : ComponentBase, IAsyncDisposable
         await base.OnParametersSetAsync().ConfigureAwait(true);
     }
 
+    private async ValueTask OnBeforeInternalNavigationAsync(LocationChangingContext context)
+    {
+        if (this._persistenceContext?.HasChanges is not true)
+        {
+            return;
+        }
+
+        var isConfirmed = await this.JavaScript.InvokeAsync<bool>(
+                "window.confirm",
+                "There are unsaved changes. Are you sure you want to discard them?")
+            .ConfigureAwait(true);
+
+        if (!isConfirmed)
+        {
+            context.PreventNavigation();
+        }
+        else
+        {
+            await this.DataSource.DiscardChangesAsync().ConfigureAwait(true);
+        }
+    }
+
     private async Task LoadDataAsync(CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
 
+        this._persistenceContext = await this.DataSource.GetContextAsync().ConfigureAwait(true);
         await this.DataSource.GetOwnerAsync().ConfigureAwait(true);
         cancellationToken.ThrowIfCancellationRequested();
         var data = this.DataSource.GetAll<MonsterDefinition>()
@@ -106,72 +147,41 @@ public partial class Merchants : ComponentBase, IAsyncDisposable
         await this.InvokeAsync(async () => await this._itemPagination.SetCurrentPageIndexAsync(0).ConfigureAwait(true)).ConfigureAwait(true);
     }
 
-    private async Task OnRemoveItemClickAsync(Item item)
+    private async Task OnSaveButtonClickAsync()
     {
-        var context = await this.DataSource.GetContextAsync().ConfigureAwait(true);
-        if (await context.DeleteAsync(item).ConfigureAwait(true))
+        if (this._persistenceContext is { } persistenceContext)
         {
-            this._selectedMerchant?.Items.Remove(item);
+            await persistenceContext.SaveChangesAsync().ConfigureAwait(true);
         }
-
-        await context.SaveChangesAsync().ConfigureAwait(true);
     }
 
-    private async Task OnCreateItemClickAsync()
+    private async Task OnCancelButtonClickAsync()
     {
-        using var tempContext = this.ContextProvider.CreateNewContext();
-        var tempItem = tempContext.CreateNew<Item>();
-        var parameters = new ModalParameters();
-        parameters.Add(nameof(ModalCreateNew<Item>.Item), tempItem);
-        parameters.Add(nameof(ModalCreateNew<Item>.PersistenceContext), tempContext);
-        var options = new ModalOptions
+        if (this._persistenceContext?.HasChanges is true)
         {
-            DisableBackgroundCancel = true,
-            Class = "blazored-modal modal-edit-item",
-        };
-
-        var modal = this.ModalService.Show<ModalCreateNew<Item>>($"Create item for {this._selectedMerchant!.Merchant.Designation}", parameters, options);
-        var result = await modal.Result.ConfigureAwait(true);
-
-        if (result.Cancelled)
-        {
-            return;
+            await this.DataSource.DiscardChangesAsync().ConfigureAwait(true);
+            await this.LoadDataAsync(default).ConfigureAwait(true);
         }
-
-        var context = await this.DataSource.GetContextAsync().ConfigureAwait(true);
-        var createdItem = context.CreateNew<Item>();
-        createdItem.AssignValues(tempItem);
-        this._selectedMerchant.Items.Add(createdItem);
-        await context.SaveChangesAsync().ConfigureAwait(true);
     }
 
-    private async Task OnEditItemClickAsync(Item item)
+    private async Task OnBackButtonClickAsync()
     {
-        var context = await this.DataSource.GetContextAsync().ConfigureAwait(true);
-        var parameters = new ModalParameters();
-        parameters.Add(nameof(ModalCreateNew<Item>.Item), item);
-        parameters.Add(nameof(ModalCreateNew<Item>.PersistenceContext), context);
-        var options = new ModalOptions
+        if (this._persistenceContext?.HasChanges is true)
         {
-            DisableBackgroundCancel = true,
-            Class = "blazored-modal modal-edit-item",
-        };
+            var isConfirmed = await this.JavaScript.InvokeAsync<bool>(
+                    "window.confirm",
+                    "There are unsaved changes. Are you sure you want to discard them?")
+                .ConfigureAwait(true);
 
-        var modal = this.ModalService.Show<ModalCreateNew<Item>>($"Edit item of {this._selectedMerchant!.Merchant.Designation}", parameters, options);
-        var result = await modal.Result.ConfigureAwait(true);
-
-        if (result.Cancelled)
-        {
-            if (context.HasChanges)
+            if (!isConfirmed)
             {
-                await this.DataSource.DiscardChangesAsync().ConfigureAwait(true);
-                await this.LoadDataAsync(default).ConfigureAwait(true);
+                return;
             }
 
-            return;
+            await this.OnCancelButtonClickAsync().ConfigureAwait(true);
         }
 
-        await context.SaveChangesAsync().ConfigureAwait(true);
+        this._selectedMerchant = null;
     }
 
     /// <summary>
