0495184edd6059cf6164ccc4a47c9d6281ec2ccd
Julian Casaburi <juliancasaburi@gmail.com>
2023-08-03 05:27:59 -0300

    fix: file header names

--- src/Persistence/BaseRepositoryProvider.cs
+++ src/Persistence/BaseRepositoryProvider.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="BaseRepositoryManager.cs" company="MUnique">
+﻿// <copyright file="BaseRepositoryProvider.cs" company="MUnique">
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
