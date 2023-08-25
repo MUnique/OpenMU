78e5e3c149ca9bcd8880be5fb67e4908b34083fa
sven-n <sven-n@users.noreply.github.com>
2023-08-25 21:16:12 +0200

    Fixed cref

--- src/Pathfinding/NodeIndexer.cs
+++ src/Pathfinding/NodeIndexer.cs
@@ -7,7 +7,7 @@ namespace MUnique.OpenMU.Pathfinding;
 /// <summary>
 /// An indexer for a <see cref="Node"/>, which can be used to calculate the index on the binary heap.
 /// The index is the predicted total cost to reach the node, divided by 10. So every 10 values,
-/// there is one index entry in the <see cref="IndexedLinkedList{T}.index"/>.
+/// there is one index entry in the <see cref="IndexedLinkedList{T}._index"/>.
 /// </summary>
 internal class NodeIndexer : IIndexer<Node>
 {
