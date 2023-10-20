7eb852f4ea27658a75549a29ad3c7548987d3b1e
sven-n <sven-n@users.noreply.github.com>
2023-10-20 19:53:29 +0200

    Made the time configurable

---
+++ src/GameLogic/PlugIns/SelfDefensePlugInConfiguration.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="SelfDefensePlugInConfiguration.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.PlugIns;
+
+/// <summary>
+/// Configuration for the <see cref="SelfDefensePlugIn"/>.
+/// </summary>
+public class SelfDefensePlugInConfiguration
+{
+    /// <summary>
+    /// Gets or sets the self defense timeout.
+    /// </summary>
+    public TimeSpan SelfDefenseTimeOut { get; set; } = TimeSpan.FromMinutes(1);
+}
