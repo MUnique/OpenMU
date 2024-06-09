86bd13a2d15e6eb2153bd367d389d40abd0d87f8
sven-n <sven-n@users.noreply.github.com>
2024-06-09 15:23:10 +0200

    removed unused using

--- src/GameLogic/Resets/ResetConfiguration.cs
+++ src/GameLogic/Resets/ResetConfiguration.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using MUnique.OpenMU.DataModel.Composition;
-
 namespace MUnique.OpenMU.GameLogic.Resets;
 
 /// <summary>
