9dfb606f99f7a6f8235374d118ec7f1b6dbdf70e
sven-n <sven-n@users.noreply.github.com>
2024-07-14 12:48:25 +0200

    Fixed entrance to balgass refugee

---
+++ src/GameLogic/Resets/GatekeeperNpcPlugin.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="GatekeeperNpcPlugin.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameLogic.Resets;
+
+using System.Runtime.InteropServices;
+using MUnique.OpenMU.GameLogic.NPC;
+using MUnique.OpenMU.GameLogic.PlugIns;
+using MUnique.OpenMU.PlugIns;
+
+/// <summary>
+/// Handles the gatekeeper npc in the Barracks of Balgass.
+/// </summary>
+[Guid("1B7BCA14-3124-4550-94B4-3FFCEE1FD55A")]
+[PlugIn(nameof(GatekeeperNpcPlugin), "Handle Gatekeeper NPC Request")]
+public class GatekeeperNpcPlugin : IPlayerTalkToNpcPlugIn
+{
+    /// <summary>
+    /// Gets the NPC number of 'Gatekeeper' in Barracks of Balgass.
+    /// </summary>
+    public static short GatekeeperNpcNumber => 408;
+
+    /// <inheritdoc />
+    public async ValueTask PlayerTalksToNpcAsync(Player player, NonPlayerCharacter npc, NpcTalkEventArgs eventArgs)
+    {
+        if (npc.Definition.Number != GatekeeperNpcNumber)
+        {
+            return;
+        }
+
+        // The client opens the dialog itself, so we don't need to do anything here.
+        eventArgs.HasBeenHandled = true;
+        eventArgs.LeavesDialogOpen = true;
+    }
+}
---
+++ src/GameServer/MessageHandler/NpcActionGroupHandlerPlugIn.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="NpcActionGroupHandlerPlugIn.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.GameServer.MessageHandler;
+
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Logging;
+using MUnique.OpenMU.PlugIns;
+
+/// <summary>
+/// Packet handler for character packets (0xF3 identifier).
+/// </summary>
+[PlugIn("NPC Action Packet Handler", "Packet handler for npc action packets (0xD0 identifier).")]
+[Guid("519874CB-734D-4061-9FCF-96B833250436")]
+internal class NpcActionGroupHandlerPlugIn : GroupPacketHandlerPlugIn
+{
+    /// <summary>
+    /// The group key.
+    /// </summary>
+    internal const byte GroupKey = 0xD0;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NpcActionGroupHandlerPlugIn" /> class.
+    /// </summary>
+    /// <param name="clientVersionProvider">The client version provider.</param>
+    /// <param name="manager">The manager.</param>
+    /// <param name="loggerFactory">The logger factory.</param>
+    public NpcActionGroupHandlerPlugIn(IClientVersionProvider clientVersionProvider, PlugInManager manager, ILoggerFactory loggerFactory)
+        : base(clientVersionProvider, manager, loggerFactory)
+    {
+    }
+
+    /// <inheritdoc/>
+    public override bool IsEncryptionExpected => false;
+
+    /// <inheritdoc/>
+    public override byte Key => GroupKey;
+}
