b6823874105ff37a7161bc75136cbd6ae80bb85d
sven-n <sven-n@users.noreply.github.com>
2024-07-14 17:52:38 +0200

    Fixed (ancient) set options They were not added to the GameConfiguration, so they couldn't be loaded correctly afterwards.

---
+++ src/Persistence/EntityFramework/Migrations/20240714154210_FixSetOptions.cs
@@ -0,0 +1,23 @@
+﻿#nullable disable
+
+namespace MUnique.OpenMU.Persistence.EntityFramework.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class FixSetOptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                                 UPDATE config."ItemOptionDefinition" SET "GameConfigurationId" = (SELECT "Id" from config."GameConfiguration")
+                                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
