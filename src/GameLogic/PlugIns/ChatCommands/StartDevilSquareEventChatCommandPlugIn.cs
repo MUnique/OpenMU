610968cf05a57c12d5be780f469a0107d0a2d88b
bernat <bvadell@feimsoft.com>
2024-08-17 18:28:50 +0200

    minor fixs

--- src/GameLogic/PlugIns/ChatCommands/StartDevilSquareEventChatCommandPlugIn.cs
+++ src/GameLogic/PlugIns/ChatCommands/StartDevilSquareEventChatCommandPlugIn.cs
@@ -9,7 +9,7 @@ using MUnique.OpenMU.GameLogic.PlugIns.PeriodicTasks;
 using MUnique.OpenMU.PlugIns;
 
 /// <summary>
-/// A chat command plugin which handles the startcc command.
+/// A chat command plugin which handles the startds command.
 /// </summary>
 [Guid("3684DC79-D81E-4033-AB2C-537334CF0BB6")]
 [PlugIn(nameof(StartDevilSquareEventChatCommandPlugIn), "Handles the chat command '/startds'. Starts the devil square event at the next possible time.")]
@@ -27,7 +27,7 @@ public class StartDevilSquareEventChatCommandPlugIn : IChatCommandPlugIn
     /// <inheritdoc />
     public async ValueTask HandleCommandAsync(Player player, string command)
     {
-        var DevilSquare = player.GameContext.PlugInManager.GetStrategy<MiniGameType, IPeriodicMiniGameStartPlugIn>(MiniGameType.DevilSquare);
-        DevilSquare?.ForceStart();
+        var devilSquare = player.GameContext.PlugInManager.GetStrategy<MiniGameType, IPeriodicMiniGameStartPlugIn>(MiniGameType.DevilSquare);
+        devilSquare?.ForceStart();
     }
 }
