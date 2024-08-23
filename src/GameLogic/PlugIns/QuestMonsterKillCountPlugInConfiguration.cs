e836b24126a53204cfbf0df2da8857eea2f6be4a
bernat <bvadell@feimsoft.com>
2024-08-23 14:05:12 +0200

    fix docs

--- src/GameLogic/PlugIns/QuestMonsterKillCountPlugInConfiguration.cs
+++ src/GameLogic/PlugIns/QuestMonsterKillCountPlugInConfiguration.cs
@@ -1,11 +1,11 @@
-﻿// <copyright file="PeriodicSaveProgressPlugInConfiguration.cs" company="MUnique">
+﻿// <copyright file="QuestMonsterKillCountPlugInConfiguration.cs" company="MUnique">
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
 namespace MUnique.OpenMU.GameLogic.PlugIns;
 
 /// <summary>
-/// Configuration for the <see cref="PeriodicSaveProgressPlugIn"/>.
+/// Configuration for the <see cref="QuestMonsterKillCountPlugInConfiguration"/>.
 /// </summary>
 public class QuestMonsterKillCountPlugInConfiguration
 {
