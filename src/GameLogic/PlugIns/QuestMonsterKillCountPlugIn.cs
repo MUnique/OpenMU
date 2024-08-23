33fe2274df0904a6e6d78ede765a08dfde3d0b03
bernat <bvadell@feimsoft.com>
2024-08-23 13:58:55 +0200

    feat: add custom message on defeat monster for quest

--- src/GameLogic/PlugIns/QuestMonsterKillCountPlugIn.cs
+++ src/GameLogic/PlugIns/QuestMonsterKillCountPlugIn.cs
@@ -6,6 +6,8 @@ namespace MUnique.OpenMU.GameLogic.PlugIns;
 
 using System.Runtime.InteropServices;
 using MUnique.OpenMU.GameLogic.NPC;
+using MUnique.OpenMU.GameLogic.PlugIns.ChatCommands;
+using MUnique.OpenMU.Persistence;
 using MUnique.OpenMU.PlugIns;
 
 /// <summary>
@@ -13,8 +15,11 @@ using MUnique.OpenMU.PlugIns;
 /// </summary>
 [PlugIn("Count killed quest monsters", "This plugin increases the monster kill count of the quest state of active quests.")]
 [Guid("416C2231-D7FE-414A-9321-26622E262EF5")]
-public class QuestMonsterKillCountPlugIn : IAttackableGotKilledPlugIn
+public class QuestMonsterKillCountPlugIn : IAttackableGotKilledPlugIn, ISupportCustomConfiguration<QuestMonsterKillCountPlugInConfiguration>, ISupportDefaultCustomConfiguration
 {
+    /// <inheritdoc/>
+    public QuestMonsterKillCountPlugInConfiguration? Configuration { get; set; }
+
     /// <summary>
     /// Is called when an <see cref="IAttackable" /> object got killed by another.
     /// </summary>
@@ -22,6 +27,8 @@ public class QuestMonsterKillCountPlugIn : IAttackableGotKilledPlugIn
     /// <param name="killer">The killer.</param>
     public async ValueTask AttackableGotKilledAsync(IAttackable killed, IAttacker? killer)
     {
+        var configuration = this.Configuration ??= CreateDefaultConfiguration();
+
         if (!(killer is Player player && killed is Monster monster)
             || player.SelectedCharacter?.QuestStates is null)
         {
@@ -46,7 +53,30 @@ public class QuestMonsterKillCountPlugIn : IAttackableGotKilledPlugIn
                 }
 
                 requirementState!.KillCount++;
+
+                if (killRequirement.MinimumNumber >= requirementState!.KillCount)
+                {
+                    var message = string.Format(
+                        configuration.Message,
+                        questState.ActiveQuest.Name,
+                        monster.Definition.Designation,
+                        requirementState.KillCount,
+                        killRequirement.MinimumNumber);
+
+                    await player.ShowMessageAsync(message).ConfigureAwait(false);
+                }
             }
         }
+    }
+
+    /// <inheritdoc/>
+    public object CreateDefaultConfig()
+    {
+        return CreateDefaultConfiguration();
+    }
+
+    private static QuestMonsterKillCountPlugInConfiguration CreateDefaultConfiguration()
+    {
+        return new QuestMonsterKillCountPlugInConfiguration();
     }
 }
