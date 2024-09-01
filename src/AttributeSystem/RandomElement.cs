65bdba239a22f51a83329893dc5377d5af0d19b1
bernat <bvadell@feimsoft.com>
2024-09-01 14:32:16 +0200

    implement RandomElement

---
+++ src/AttributeSystem/RandomElement.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="RandomElement.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.AttributeSystem;
+
+/// <summary>
+/// An element with a constant value.
+/// </summary>
+public class RandomElement : IElement
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RandomElement" /> class.
+    /// </summary>
+    /// <param name="minValue">The min value.</param>
+    /// <param name="maxValue">The max value.</param>
+    /// <param name="aggregateType">Type of the aggregate.</param>
+    public RandomElement(float minValue, float maxValue, AggregateType aggregateType = AggregateType.AddRaw)
+    {
+        this.MinValue = minValue;
+        this.MaxValue = maxValue;
+        this.AggregateType = aggregateType;
+    }
+
+    /// <summary>
+    /// Never occurs, so the implementation is empty.
+    /// </summary>
+    public event EventHandler? ValueChanged
+    {
+        add
+        {
+            // do nothing, as the value never changes.
+        }
+
+        remove
+        {
+            // do nothing, as the value never changes.
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the min value.
+    /// </summary>
+    public float MinValue { get; set; }
+
+    /// <summary>
+    /// Gets or sets the max value.
+    /// </summary>
+    public float MaxValue { get; set; }
+
+    /// <inheritdoc/>
+    public float Value
+    {
+        get
+        {
+            double randomValue = Random.Shared.NextDouble();
+            return (float)(this.MinValue + (randomValue * (this.MaxValue - this.MinValue)));
+        }
+    }
+
+    /// <inheritdoc/>
+    public AggregateType AggregateType { get; }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        return $"{this.Value} ({this.AggregateType})";
+    }
+}
---
+++ tests/MUnique.OpenMU.AttributeSystem.Tests/RandomElementTests.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="RandomElementTests.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.AttributeSystem.Tests;
+
+/// <summary>
+/// Tests for the <see cref="RandomElementTests"/>.
+/// </summary>
+[TestFixture]
+public class RandomElementTests
+{
+    /// <summary>
+    /// Tests if the value of the element is as defined in the constructor.
+    /// </summary>
+    [Test]
+    public void ValueAsDefined()
+    {
+        const int minValue = 4;
+        const int maxValue = 10;
+        var element = new RandomElement(minValue, maxValue);
+        var randomValue = element.Value;
+        Assert.That(randomValue, Is.GreaterThanOrEqualTo(minValue));
+        Assert.That(randomValue, Is.LessThanOrEqualTo(maxValue));
+    }
+}
