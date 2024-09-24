94b44e8ebbcf0f236993407494315fd46a982b61
sven-n <sven-n@users.noreply.github.com>
2024-09-24 19:57:13 +0200

    Improved AdminPanel UI to manage connect servers

---
+++ src/Dapr/AdminPanel.Host/DockerConnectServerInstanceManager.cs
@@ -0,0 +1,40 @@
+﻿using MUnique.OpenMU.Interfaces;
+
+namespace MUnique.OpenMU.AdminPanel.Host;
+
+/// <summary>
+/// An implementation of <see cref="IConnectServerInstanceManager"/>.
+/// </summary>
+public class DockerConnectServerInstanceManager : IConnectServerInstanceManager
+{
+    private readonly IServerProvider _serverProvider;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DockerGameServerInstanceManager"/> class.
+    /// </summary>
+    /// <param name="serverProvider">The server provider.</param>
+    public DockerConnectServerInstanceManager(IServerProvider serverProvider)
+    {
+        this._serverProvider = serverProvider;
+    }
+
+    /// <inheritdoc />
+    public async ValueTask InitializeConnectServerAsync(byte serverId)
+    {
+        // TODO: Implement this... by starting a new docker container
+
+    }
+
+    /// <inheritdoc />
+    public async ValueTask RemoveConnectServerAsync(byte serverId)
+    {
+        var connectServers = this._serverProvider.Servers
+            .Where(server => server.Type == ServerType.ConnectServer)
+            .FirstOrDefault(server => server.Id == serverId);
+        if (connectServers is not null)
+        {
+            await connectServers.ShutdownAsync().ConfigureAwait(false);
+            // TODO: Remove the docker container
+        }
+    }
+}
---
+++ src/Interfaces/IConnectServerInstanceManager.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="IConnectServerInstanceManager.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Interfaces;
+
+/// <summary>
+/// Interface for an instance which manages connect servers.
+/// </summary>
+public interface IConnectServerInstanceManager
+{
+    /// <summary>
+    /// Initializes a connect server with the specified definition.
+    /// </summary>
+    /// <param name="connectServerDefinitionId">The connect server definition identifier.</param>
+    ValueTask InitializeConnectServerAsync(Guid connectServerDefinitionId);
+
+    /// <summary>
+    /// Removes the connect server instance of the specified definition.
+    /// </summary>
+    /// <param name="connectServerDefinitionId">The connect server definition identifier.</param>
+    ValueTask RemoveConnectServerAsync(Guid connectServerDefinitionId);
+}
