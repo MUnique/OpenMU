e81706b60ae4574c3fa2f4273017063db5ae1cd9
sven-n <sven-n@users.noreply.github.com>
2024-10-24 22:01:04 +0200

    Added migration

--- src/Persistence/EntityFramework/Migrations/20241021175011_AddAreaSkillConfiguration.Designer.cs
+++ src/Persistence/EntityFramework/Migrations/20241021175011_AddAreaSkillConfiguration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MUnique.OpenMU.Persistence.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MUnique.OpenMU.Persistence.EntityFramework.Migrations
 {
     [DbContext(typeof(EntityDataContext))]
-    partial class EntityDataContextModelSnapshot : ModelSnapshot
+    [Migration("20241021175011_AddAreaSkillConfiguration")]
+    partial class AddAreaSkillConfiguration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -179,6 +182,9 @@ namespace MUnique.OpenMU.Persistence.EntityFramework.Migrations
 
                     b.Property<Guid?>("GameConfigurationId")
                         .HasColumnType("uuid");
+
+                    b.Property<float?>("MaximumValue")
+                        .HasColumnType("real");
 
                     b.HasKey("Id");
 
