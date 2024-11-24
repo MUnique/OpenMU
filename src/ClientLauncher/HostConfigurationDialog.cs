eb0ba6f04fa2fe2db5b89a0a001d08b1d6b8bdbb
sven-n <sven-n@users.noreply.github.com>
2024-11-24 16:14:13 +0100

    fixed using order

--- src/ClientLauncher/HostConfigurationDialog.cs
+++ src/ClientLauncher/HostConfigurationDialog.cs
@@ -1,10 +1,10 @@
-﻿using System.ComponentModel;
-// <copyright file="HostConfigurationDialog.cs" company="MUnique">
+﻿// <copyright file="HostConfigurationDialog.cs" company="MUnique">
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 // </copyright>
 
 namespace MUnique.OpenMU.ClientLauncher;
 
+using System.ComponentModel;
 using System.Windows.Forms;
 
 /// <summary>
