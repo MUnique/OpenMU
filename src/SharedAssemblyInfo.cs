a036002ecb4a6a057e7e352f561a453b5abd5f19
sven-n <sven-n@users.noreply.github.com>
2025-06-25 21:34:39 +0200

    Fixed DuelStartResponse packet length

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MUnique")]
 [assembly: AssemblyProduct("OpenMU")]
-[assembly: AssemblyCopyright("Copyright © MUnique 2024")]
+[assembly: AssemblyCopyright("Copyright © MUnique 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.7.0")]
-[assembly: AssemblyFileVersion("0.9.7.0")]
+[assembly: AssemblyVersion("0.9.8.0")]
+[assembly: AssemblyFileVersion("0.9.8.0")]
