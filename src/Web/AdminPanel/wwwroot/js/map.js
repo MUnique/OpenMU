73924072d522e2fc8b457c4aced3c4116d7ded38
sven-n <sven-n@users.noreply.github.com>
2025-12-22 18:52:18 +0100

    Upgrade build target to .net 10  and solved static asset issue

--- src/Web/AdminPanel/wwwroot/js/map.js
+++
@@ -1,5 +0,0 @@
-﻿// plain javascript to make JS Interop easier
-
-function GetMapHostBoundingClientRect() {
-    return document.getElementsByClassName("map-host")[0].getBoundingClientRect();
-}
---
+++ src/Web/Shared/Exports.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="Exports.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Web.Shared;
+
+using System.Collections.Immutable;
+
+/// <summary>
+/// Class which holds the script exports of this project.
+/// </summary>
+/// <remarks>
+/// TODO: Instead of a static class, create an interface, so we can inject an instance into the layout.
+///       For example, we could further add some common Components which render the Scripts, Stylesheets, etc.
+/// </remarks>
+public static class Exports
+{
+    /// <summary>
+    /// Gets the url prefix to the scripts of this project.
+    /// </summary>
+    private static string Prefix { get; } = $"_content/{typeof(Exports).Namespace}";
+
+    private static IEnumerable<string> SharedScripts
+    {
+        get
+        {
+            yield return "_content/Blazored.Typeahead/blazored-typeahead.js";
+            //yield return "_content/Blazored.Modal/blazored.modal.js";
+            yield return "_content/BlazorInputFile/inputfile.js";
+        }
+    }
+
+    private static IEnumerable<string> SharedStylesheets
+    {
+        get
+        {
+            yield return "_content/Blazored.Typeahead/blazored-typeahead.css";
+            //yield return "_content/Blazored.Modal/blazored-modal.css";
+            yield return $"_content/MUnique.OpenMU.Web.Shared/css/shared.css";
+        }
+    }
+
+    /// <summary>
+    /// Gets the scripts.
+    /// </summary>
+    public static ImmutableList<string> Scripts { get; } = SharedScripts.ToImmutableList();
+
+    /// <summary>
+    /// Gets the script mappings.
+    /// </summary>
+    public static ImmutableList<(string Key, string Path)> ScriptMappings { get; } = [];
+
+    /// <summary>
+    /// Gets the stylesheets.
+    /// </summary>
+    public static ImmutableList<string> Stylesheets { get; } = SharedStylesheets.ToImmutableList();
+}
