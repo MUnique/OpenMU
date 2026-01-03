7a5bd60dcbdb09a91a512889e68c00eef55fd1c3
emanuel catania <82476648+EmanuelCatania@users.noreply.github.com>
2026-01-02 23:10:51 -0300

    fix evo build

--- src/Persistence/Initialization/Version097d/CharacterClassInitialization.cs
+++ src/Persistence/Initialization/Version097d/CharacterClassInitialization.cs
@@ -5,7 +5,6 @@
 namespace MUnique.OpenMU.Persistence.Initialization.Version097d;
 
 using MUnique.OpenMU.DataModel.Configuration;
-using MUnique.OpenMU.GameLogic.Attributes;
 using MUnique.OpenMU.Persistence.Initialization.CharacterClasses;
 
 /// <summary>
@@ -29,54 +28,15 @@ internal class CharacterClassInitialization : Initialization.CharacterClasses.Ch
     /// <inheritdoc />
     public override void Initialize()
     {
-        var bladeMaster = this.CreateBladeMaster();
-        var bladeKnight = this.CreateBladeKnight(bladeMaster);
+        var bladeKnight = this.CreateDarkKnight(CharacterClassNumber.BladeKnight, "Blade Knight", false, null, false);
         this.CreateDarkKnight(CharacterClassNumber.DarkKnight, "Dark Knight", false, bladeKnight, true);
 
-        var grandMaster = this.CreateGrandMaster();
-        var soulMaster = this.CreateSoulMaster(grandMaster);
+        var soulMaster = this.CreateDarkWizard(CharacterClassNumber.SoulMaster, "Soul Master", false, null, false);
         this.CreateDarkWizard(CharacterClassNumber.DarkWizard, "Dark Wizard", false, soulMaster, true);
 
-        var highElf = this.CreateHighElf();
-        var museElf = this.CreateMuseElf(highElf);
+        var museElf = this.CreateFairyElf(CharacterClassNumber.MuseElf, "Muse Elf", false, null, false);
         this.CreateFairyElf(CharacterClassNumber.FairyElf, "Fairy Elf", false, museElf, true);
 
         this.CreateMagicGladiator(CharacterClassNumber.MagicGladiator, "Magic Gladiator", false, null, true);
-    }
-
-    private CharacterClass CreateBladeMaster()
-    {
-        var result = this.CreateDarkKnight(CharacterClassNumber.BladeMaster, "Blade Master", true, null, false);
-        result.StatAttributes.Add(this.CreateStatAttributeDefinition(Stats.MasterLevel, 0, false));
-        return result;
-    }
-
-    private CharacterClass CreateBladeKnight(CharacterClass bladeMaster)
-    {
-        return this.CreateDarkKnight(CharacterClassNumber.BladeKnight, "Blade Knight", false, bladeMaster, false);
-    }
-
-    private CharacterClass CreateGrandMaster()
-    {
-        var result = this.CreateDarkWizard(CharacterClassNumber.GrandMaster, "Grand Master", true, null, false);
-        result.StatAttributes.Add(this.CreateStatAttributeDefinition(Stats.MasterLevel, 0, false));
-        return result;
-    }
-
-    private CharacterClass CreateSoulMaster(CharacterClass grandMaster)
-    {
-        return this.CreateDarkWizard(CharacterClassNumber.SoulMaster, "Soul Master", false, grandMaster, false);
-    }
-
-    private CharacterClass CreateHighElf()
-    {
-        var result = this.CreateFairyElf(CharacterClassNumber.HighElf, "High Elf", true, null, false);
-        result.StatAttributes.Add(this.CreateStatAttributeDefinition(Stats.MasterLevel, 0, false));
-        return result;
-    }
-
-    private CharacterClass CreateMuseElf(CharacterClass highElf)
-    {
-        return this.CreateFairyElf(CharacterClassNumber.MuseElf, "Muse Elf", false, highElf, false);
     }
 }
