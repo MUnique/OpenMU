01881626296b2dc8d8ed80f9506ba148b9ada54a
sven-n <sven-n@users.noreply.github.com>
2026-01-06 22:25:32 +0100

    Implement IAssignable for PlugInConfiguration

--- src/Persistence/EntityFramework/Model/PlugInConfiguration.Custom.cs
+++ src/Persistence/EntityFramework/Model/PlugInConfiguration.Custom.cs
@@ -4,11 +4,41 @@
 
 namespace MUnique.OpenMU.Persistence.EntityFramework.Model;
 
+using MUnique.OpenMU.DataModel;
+
 /// <summary>
 /// Custom additions to the generated <see cref="PlugInConfiguration"/>.
 /// </summary>
-internal partial class PlugInConfiguration
+internal partial class PlugInConfiguration : IAssignable, IAssignable<MUnique.OpenMU.PlugIns.PlugInConfiguration>, ICloneable<MUnique.OpenMU.PlugIns.PlugInConfiguration>
 {
+    /// <inheritdoc />
+    public virtual MUnique.OpenMU.PlugIns.PlugInConfiguration Clone(DataModel.Configuration.GameConfiguration gameConfiguration)
+    {
+        var clone = new PlugInConfiguration();
+        clone.AssignValuesOf(this, gameConfiguration);
+        return clone;
+    }
+
+    /// <inheritdoc />
+    public virtual void AssignValuesOf(object other, MUnique.OpenMU.DataModel.Configuration.GameConfiguration gameConfiguration)
+    {
+        if (other is MUnique.OpenMU.Persistence.EntityFramework.Model.PlugInConfiguration typedOther)
+        {
+            this.AssignValuesOf(typedOther, gameConfiguration);
+        }
+    }
+
+    /// <inheritdoc />
+    public virtual void AssignValuesOf(MUnique.OpenMU.PlugIns.PlugInConfiguration other, MUnique.OpenMU.DataModel.Configuration.GameConfiguration gameConfiguration)
+    {
+        this.Id = other.GetId();
+        this.TypeId = other.TypeId;
+        this.IsActive = other.IsActive;
+        this.CustomPlugInSource = other.CustomPlugInSource;
+        this.ExternalAssemblyName = other.ExternalAssemblyName;
+        this.CustomConfiguration = other.CustomConfiguration;
+    }
+
     /// <inheritdoc />
     public override string ToString()
     {
