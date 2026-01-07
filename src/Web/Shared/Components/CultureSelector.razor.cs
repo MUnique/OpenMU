90c9dcd789361d90d318a1041523952de6b892d5
sven-n <sven-n@users.noreply.github.com>
2026-01-07 22:15:45 +0100

    Implemented a culture selector component

---
+++ src/Web/Shared/Components/CultureSelector.razor.cs
@@ -0,0 +1,72 @@
+﻿// <copyright file="CultureSelector.razor.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Web.Shared.Components;
+
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
+using Microsoft.JSInterop;
+using MUnique.OpenMU.Web.Shared.Services;
+
+/// <summary>
+/// A selection component for supported UI-Cultures (languages).
+/// </summary>
+/// <seealso cref="Microsoft.AspNetCore.Components.ComponentBase" />
+public partial class CultureSelector
+{
+    private readonly IJSRuntime _jsRuntime;
+    private readonly NavigationManager _navigationManager;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CultureSelector"/> class.
+    /// </summary>
+    /// <param name="localizationOptions">The localization options.</param>
+    /// <param name="jsRuntime">The js runtime.</param>
+    /// <param name="navigationManager">The navigation manager.</param>
+    public CultureSelector(IOptions<RequestLocalizationOptions> localizationOptions, IJSRuntime jsRuntime, NavigationManager navigationManager)
+    {
+        this._jsRuntime = jsRuntime;
+        this._navigationManager = navigationManager;
+        this.SupportedUICultures = localizationOptions.Value.SupportedUICultures?.OrderBy(c => c.NativeName).ToList() ?? [];
+    }
+
+    /// <summary>
+    /// Gets or sets the selected culture code.
+    /// </summary>
+    public string SelectedCultureCode
+    {
+        get => CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+        set
+        {
+            if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == value)
+            {
+                return;
+            }
+
+            CultureInfo.CurrentUICulture = new CultureInfo(value);
+            this.SetCultureInCookie();
+        }
+    }
+
+    /// <summary>
+    /// Gets the supported UI cultures.
+    /// </summary>
+    private IList<CultureInfo> SupportedUICultures { get; }
+
+    /// <summary>
+    /// Sets the culture in a cookie by calling the <see cref="CultureController"/>.
+    /// </summary>
+    private void SetCultureInCookie()
+    {
+        var uri = new Uri(this._navigationManager.Uri)
+            .GetComponents(UriComponents.PathAndQuery, UriFormat.Unescaped);
+        var cultureEscaped = Uri.EscapeDataString(CultureInfo.CurrentUICulture.Name);
+        var uriEscaped = Uri.EscapeDataString(uri);
+
+        var fullUri = $"Culture/Set?culture={cultureEscaped}&redirectUri={uriEscaped}";
+        this._navigationManager.NavigateTo(fullUri, forceLoad: true);
+    }
+}
---
+++ src/Web/Shared/Services/CultureController.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="CultureController.cs" company="MUnique">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace MUnique.OpenMU.Web.Shared.Services;
+
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// A controller which sets the culture to the specified value and redirects to the specified uri.
+/// </summary>
+[Route("[controller]/[action]")]
+public class CultureController : Controller
+{
+    /// <summary>
+    /// Sets the current UI culture by writing a culture cookie and redirects to the specified URI.
+    /// </summary>
+    /// <param name="culture">The culture name to set, for example &quot;en-US&quot; or &quot;de-DE&quot;. If <see langword="null"/>, the culture is not changed.</param>
+    /// <param name="redirectUri">The URI to which the client should be redirected after the culture has been set.</param>
+    /// <returns>
+    /// An <see cref="IActionResult"/> that performs a local redirect to the specified <paramref name="redirectUri"/>.
+    /// </returns>
+    public IActionResult Set(string? culture, string redirectUri)
+    {
+        if (culture is not null)
+        {
+            var requestCulture = new RequestCulture(culture, culture);
+            var cookieName = CookieRequestCultureProvider.DefaultCookieName;
+            var cookieValue = CookieRequestCultureProvider.MakeCookieValue(requestCulture);
+
+            this.HttpContext.Response.Cookies.Append(cookieName, cookieValue);
+        }
+
+        return this.LocalRedirect(redirectUri);
+    }
+}
