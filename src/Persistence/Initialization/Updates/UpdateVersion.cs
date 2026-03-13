3c5749d5ce91a55c1c42dd6e07e8be458bc50085
Oleksii Lisikh <alisiikh@gmail.com>
2026-03-13 12:56:12 +0100

    feat(persistence): add Crest of Monarch drop group in Icarus

--- src/Persistence/Initialization/Updates/UpdateVersion.cs
+++ src/Persistence/Initialization/Updates/UpdateVersion.cs
@@ -344,4 +344,9 @@ public enum UpdateVersion
     /// The version of the <see cref="AddProjectileCountToTripleShotUpdatePlugIn"/>.
     /// </summary>
     AddProjectileCountToTripleShot = 67,
+
+    /// <summary>
+    /// The version of the <see cref="AddCrestOfMonarchDropGroupUpdateSeason6"/>.
+    /// </summary>
+    AddCrestOfMonarchDropGroupSeason6 = 68,
 }
