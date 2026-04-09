9bf30ba3933f86dc243c68d45cc3e8332e50bbc4
itayalroy <itayalroy@gmail.com>
2026-04-09 03:47:17 +0300

    Fix AreaSkillSettings for mage skills

--- src/Persistence/Initialization/Updates/UpdateVersion.cs
+++ src/Persistence/Initialization/Updates/UpdateVersion.cs
@@ -392,4 +392,9 @@ public enum UpdateVersion
     /// The version of the <see cref="FixSummonerCurseSkillsPlugIn"/>.
     /// </summary>
     FixSummonerCurseSkills = 77,
+
+    /// <summary>
+    /// The version of the <see cref="FixAreaSkillsUpdatePlugIn"/>.
+    /// </summary>
+    FixAreaSkills = 78,
 }
