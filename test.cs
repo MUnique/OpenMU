da90df45017b38074de338793dc3056554ae233a
Lalyo Boyadzhiev <lalyo.boyadzhiev@gmail.com>
2026-07-23 22:21:13 +0200

    style(golden-archer): remove temporary test.cs file

--- test.cs
+++
@@ -1,1 +0,0 @@
-using System; using System.Reflection; using MUnique.OpenMU.DataModel.Composition; namespace Test { class Program { static void Main() { var p = typeof(MUnique.OpenMU.Persistence.EntityFramework.Model.GameConfiguration).GetProperty("GoldenArcherConfiguration"); Console.WriteLine(p.GetCustomAttribute<MemberOfAggregateAttribute>() != null); } } }
